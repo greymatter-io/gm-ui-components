ddf0ed1a0b51fd6b6c4b676dbf4527449c3404e2
Kait Moreno <kaitmoreno@Kaits-MacBook-Pro.local>
2017-09-29 14:36:33 -0400

    refactored withInfo

--- src/stories/Breadcrumbs.story.js
+++ src/stories/Breadcrumbs.story.js
@@ -9,15 +9,16 @@ const stories = storiesOf("Breadcrumbs", module);
 const label = "Breadcrumbs";
 const defaultCrumbs = ["Home", "View", "Data", "Graphs"];
 const separator = ",";
+const breadCrumbsInfo =
+  "A React component that allows users to know their location. Use <Breadcrumbs> as the wrapper component and <BreadcrumbsItem> for rendering an individual link. The container accepts a maxItems prop that sets the amount of breadcrumbs to be rendered. If there are more children than the maximum, it will render a collapsed view.";
+
 let crumbs, maxItems;
 
 stories.addDecorator(withKnobs);
 
 stories.add(
   "simple breadcrumbs",
-  withInfo(
-    "A React component that allows users to know their location. Use <Breadcrumbs> as the wrapper component and <BreadcrumbsItem> for rendering an individual link. The container accepts a maxItems prop that sets the amount of breadcrumbs to be rendered. If there are more children than the maximum, it will render a collapsed view."
-  )(() => {
+  withInfo(breadCrumbsInfo)(() => {
     crumbs = array(label, defaultCrumbs, separator);
     return (
       <Breadcrumbs>
@@ -31,9 +32,7 @@ stories.add(
 
 stories.add(
   "truncated breadcrumbs",
-  withInfo(
-    "A React component that allows users to know their location. Use <Breadcrumbs> as the wrapper component and <BreadcrumbsItem> for rendering an individual link. The container accepts a maxItems prop that sets the amount of breadcrumbs to be rendered. If there are more children than the maximum, it will render a collapsed view."
-  )(() => {
+  withInfo(breadCrumbsInfo)(() => {
     crumbs = array(label, defaultCrumbs, separator);
     maxItems = number("Max Items", 3);
     return (
