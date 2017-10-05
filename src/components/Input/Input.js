c5470a5392ae16deedff5e8c6d10244c4f7423ba
Kait Moreno <kaitmoreno@Kaits-MacBook-Pro.local>
2017-10-05 11:54:29 -0400

    fix: refactored input CSS

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -11,25 +11,19 @@ const Input = styled.input.attrs({
   user-select: auto;
   text-align: left;
   font-size: 14px;
-  border-radius: 3px;
+  color: black;
   padding: 8px;
   font-weight: 500;
-  border: 1px solid #f0f0f0;
-  line-height: 1.4;
-  border-style: solid;
-  text-transform: none;
+  line-height: 2;
   width: ${props => (props.shouldFitContainer ? "100%" : "auto")};
+  border: 1px solid #f0f0f0;
+  border-radius: 3px;
   border-top-color: ${lighten("3%", "#ececec")};
   border-bottom-color: #cdcdcd;
-  color: black;
   box-shadow: inset 0 0 0 rgba(0, 0, 0, 0), 0 0 0 rgba(0, 0, 0, 0),
     0 0 0 rgba(0, 0, 0, 0);
   &::placeholder {
     text-align: center;
-  }
-  &::-ms-expand {
-    background-color: transparent;
-    border: 0;
   }
   &:hover {
     background-color: #fbfbfb;
