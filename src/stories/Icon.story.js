2cd998d1bba3885a68840b6db44ece18c31c1f2e
kaitmore <kaitmore@gmail.com>
2018-01-12 10:03:53 -0500

    fix: support custom svgs

--- src/stories/Icon.story.js
+++ src/stories/Icon.story.js
@@ -83,39 +83,69 @@ storiesOf("Icons", module)
     )(() => {
       return (
         <Icon
-        backgroundStyle={select(
-          "backgroundStyle",
-          iconBackgroundNames,
-          "BackgroundSquareRounded"
-        )}
-        backgroundColor={color("backgroundColor", "#000")}
-        backgroundOpacity={number("backgroundOpacity", 2)}
-        backgroundSizeRatio={number("backgroundSizeRatio", 1)}
-        borderStyle={select("borderStyle", iconBorderNames, "BorderSquare")}
-        borderColor={color("borderColor", "currentColor")}
-        borderOpacity={number("borderOpacity", 0.5)}
-        borderWidth={number("borderWidth", 0.1)}
-        glyphColor={color("glyphColor", "#fff")}
-        glyphSizeRatio={number("glyphSizeRatio", 1)}
-        glyph={select("glyph", glyphNames, "Bell")}
+          backgroundStyle={select(
+            "backgroundStyle",
+            iconBackgroundNames,
+            "BackgroundSquareRounded"
+          )}
+          backgroundColor={color("backgroundColor", "#000")}
+          backgroundOpacity={number("backgroundOpacity", 1)}
+          backgroundSizeRatio={number("backgroundSizeRatio", 1)}
+          borderStyle={select("borderStyle", iconBorderNames, "BorderSquare")}
+          borderColor={color("borderColor", "currentColor")}
+          borderOpacity={number("borderOpacity", 0.5)}
+          borderWidth={number("borderWidth", 0.1)}
+          glyphColor={color("glyphColor", "#fff")}
+          glyphSizeRatio={number("glyphSizeRatio", 1)}
+          glyph={select("glyph", glyphNames, "Bell")}
         />
       );
     })
   )
+  .add(
+    "custom glyph",
+    withInfo(
+      "An Icon component that renders a custom glyph. Simply wrap your custom svg with <Icon>."
+    )(() => {
+      return (
+        <Icon
+          backgroundStyle={select(
+            "backgroundStyle",
+            iconBackgroundNames,
+            "BackgroundSquareRounded"
+          )}
+          backgroundColor={color("backgroundColor", "#eee")}
+          glyphColor={color("glyphColor", "#444")}
+          backgroundOpacity={number("backgroundOpacity", 1)}
+          backgroundSizeRatio={number("backgroundSizeRatio", 1)}
+          title="Custom Glyph"
+        >
+          <svg className="svg-icon" viewBox="0 0 20 20">
+            <path d="M17.218,2.268L2.477,8.388C2.13,8.535,2.164,9.05,2.542,9.134L9.33,10.67l1.535,6.787c0.083,0.377,0.602,0.415,0.745,0.065l6.123-14.74C17.866,2.46,17.539,2.134,17.218,2.268 M3.92,8.641l11.772-4.89L9.535,9.909L3.92,8.641z M11.358,16.078l-1.268-5.613l6.157-6.157L11.358,16.078z" />
+          </svg>
+        </Icon>
+      );
+    })
+  )
   .add("gallery", () => {
-    return <div>
-    {glyphNames.map(glyph => {
-      return <div key={glyph}>
-      <span>{glyph}: </span>
-      <Icon
-          glyph={glyph}
-          backgroundStyle="BackgroundSquareRounded"
-          backgroundColor="#eee"
-          backgroundOpacity={1}
-          backgroundSizeRatio={1}
-          glyphColor="#000"
-          glyphRatio={1}
-        /></div>
-    })}
-    </div>
+    return (
+      <div>
+        {glyphNames.map(glyph => {
+          return (
+            <div key={glyph}>
+              <span>{glyph}: </span>
+              <Icon
+                glyph={glyph}
+                backgroundStyle="BackgroundSquareRounded"
+                backgroundColor="#eee"
+                backgroundOpacity={1}
+                backgroundSizeRatio={1}
+                glyphColor="#000"
+                glyphRatio={1}
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
   });
