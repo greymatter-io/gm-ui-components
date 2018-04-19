594ba0f95df1b094907b3b626f6aca88cb78b556
Kaitlin Moreno <kaitmore@gmail.com>
2018-04-19 12:12:36 -0400

    fix: update storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,73 +1,57 @@
-("use strict");
-
-require("babel-polyfill");
-const autoprefixer = require("autoprefixer");
 const path = require("path");
 const webpack = require("webpack");
-const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
-
-const WatchMissingNodeModulesPlugin = require("react-dev-utils/WatchMissingNodeModulesPlugin");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
+  entry: "./src/components/index.js",
   resolve: {
     modules: [path.resolve(__dirname, "../src"), "node_modules"],
     extensions: [".json", ".js"]
   },
   module: {
-    strictExportPresence: true,
     rules: [
-      // ** ADDING/UPDATING LOADERS **
-      // The "file" loader handles all assets unless explicitly excluded.
-      // The `exclude` list *must* be updated with every change to loader extensions.
-      // When adding a new loader, you must add its `test`
-      // as a new entry in the `exclude` list for "file" loader.
-
-      // "file" loader makes sure those assets get served by WebpackDevServer.
-      // When you `import` an asset, you get its (virtual) filename.
-      // In production, they would get copied to the `build` folder.
       {
-        exclude: [/\.html$/, /\.(js|jsx)$/, /\.css$/, /\.json$/, /\.ejs$/],
-        loader: require.resolve("file-loader"),
-        options: {
-          name: "static/media/[name].[hash:8].[ext]"
-        }
+        use: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
       },
-      // Process JS with Babel.
       {
-        test: /\.(js|jsx)$/,
-        include: [path.resolve(__dirname, "../src")],
-        loader: require.resolve("babel-loader"),
-        options: {
-          // This is a feature of `babel-loader` for webpack (not Babel itself).
-          // It enables caching results in ./node_modules/.cache/babel-loader/
-          // directory for faster rebuilds.
-          cacheDirectory: true
-        }
+        use: ["file-loader"],
+        test: /\.(jpg|gif)$/
       },
-      // "postcss" loader applies autoprefixer to our CSS.
-      // "css" loader resolves paths in CSS and adds assets as dependencies.
-      // "style" loader turns CSS into JS modules that inject <style> tags.
-      // In production, we use a plugin to extract that CSS to a file, but
-      // in development "style" loader enables hot editing of CSS.
       {
-        test: /\.(css)$/,
+        use: ["url-loader"],
+        test: /\.(eot|png|svg|ttf|woff|woff2)$/
+      },
+      {
+        test: /\.(css|scss|less)$/,
         use: [
-          require.resolve("style-loader"),
+          "style-loader",
           {
-            loader: require.resolve("css-loader"),
+            loader: "css-loader",
             options: {
-              importLoaders: 1,
-              sourceMap: true
+              importLoaders: 1
+              // sourceMap currently disabled due to a runtime overhead in
+              // development; a noticeable flicker is observed applying CSS
+              // sourceMap: true
             }
           },
+          "resolve-url-loader",
           {
-            loader: require.resolve("postcss-loader"),
+            loader: "postcss-loader",
             options: {
+              // Necessary for external CSS imports to work
               ident: "postcss",
-              sourceMap: true,
+              // sourceMap: true,
               plugins: () => [
                 require("postcss-flexbugs-fixes"),
                 autoprefixer({
+                  browsers: [
+                    ">1%",
+                    "last 4 versions",
+                    "Firefox ESR",
+                    "not ie < 9" // React doesn't support IE8 anyway
+                  ],
                   flexbox: "no-2009"
                 })
               ]
@@ -75,30 +59,6 @@ module.exports = {
           }
         ]
       }
-      // ** STOP ** Are you adding a new loader?
-      // Remember to add the new extension(s) to the "file" loader exclusion list.
     ]
-  },
-  plugins: [
-    // Add module names to factory functions so they appear in browser profiler.
-    new webpack.NamedModulesPlugin(),
-    // Watcher doesn't work well if you mistype casing in a path so we use
-    // a plugin that prints an error when you attempt to do this.
-    // See https://github.com/facebookincubator/create-react-app/issues/240
-    new CaseSensitivePathsPlugin()
-  ],
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    dgram: "empty",
-    fs: "empty",
-    net: "empty",
-    tls: "empty"
-  },
-  // Turn off performance hints during development because we don't do any
-  // splitting or minification in interest of speed. These warnings become
-  // cumbersome.
-  performance: {
-    hints: false
   }
 };
