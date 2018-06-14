213ca2509b4a669915d66927a1cf698d01fefcb1
Kaitlin Moreno <kaitmore@gmail.com>
2018-06-14 14:26:25 -0400

    test: update snaps and fix tests

--- src/components/LayoutSection/LayoutSection.test.js
+++ src/components/LayoutSection/LayoutSection.test.js
@@ -3,52 +3,42 @@ import { shallow } from "enzyme";
 import { LayoutSection, Bell, Readout, ReadoutGroup } from "../";
 
 const LayoutSectionWithReadouts = (
-  stretchContent = false,
-  stretchSection = false
-) => {
-  return (
-    <LayoutSection
-      title={"Test Layout"}
-      icon={() => <Bell size={"24px"} />}
-      stretchContent={stretchContent}
-      stretchSection={stretchSection}
-    >
-      <ReadoutGroup>
-        <Readout
-          readoutItems={[
-            {
-              title: "Uptime",
-              value: "1000ms"
-            }
-          ]}
-        />
-        <Readout
-          primary
-          readoutItems={[
-            {
-              title: "Error Rate",
-              value: "0.000%",
-              icon: () => <Bell size={"24px"} />
-            }
-          ]}
-        />
-        <Readout
-          readoutItems={[
-            {
-              title: "Host CPU Cores",
-              value: "0"
-            }
-          ]}
-        />
-      </ReadoutGroup>
-    </LayoutSection>
-  );
-};
+  <LayoutSection title={"Test Layout"} icon={() => <Bell size={"24px"} />}>
+    <ReadoutGroup>
+      <Readout
+        readoutItems={[
+          {
+            title: "Uptime",
+            value: "1000ms"
+          }
+        ]}
+      />
+      <Readout
+        primary
+        readoutItems={[
+          {
+            title: "Error Rate",
+            value: "0.000%",
+            icon: () => <Bell size={"24px"} />
+          }
+        ]}
+      />
+      <Readout
+        readoutItems={[
+          {
+            title: "Host CPU Cores",
+            value: "0"
+          }
+        ]}
+      />
+    </ReadoutGroup>
+  </LayoutSection>
+);
 
 describe("LayoutSection", () => {
   let tree;
   beforeEach(() => {
-    tree = shallow(LayoutSectionWithReadouts());
+    tree = shallow(LayoutSectionWithReadouts);
   });
   test("matches snapshot", () => {
     expect(tree).toMatchSnapshot();
@@ -66,16 +56,5 @@ describe("LayoutSection", () => {
         .children()
         .text()
     ).toBe("Test Layout");
-  });
-  test("sets flex-grow when stretchSection is true", () => {
-    tree = shallow(LayoutSectionWithReadouts(true, true));
-    expect(tree.find("LayoutSectionWrap")).toHaveStyleRule("flex", "1 0 auto");
-  });
-  test("sets align-items when stretchContent is true", () => {
-    tree = shallow(LayoutSectionWithReadouts(true, true));
-    expect(tree.find("SectionContent")).toHaveStyleRule(
-      "align-items",
-      "stretch"
-    );
   });
 });
