9fb3a827116d2a782a6567c8613f635a7f5054ac
Kait Moreno <kaitlin.moreno@deciphernow.com>
2018-06-14 09:45:53 -0400

    fix: update Input styles (#196)
    * feat: add Input component
    
    * fix: icon export

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,33 +1,12 @@
 import React from "react";
-
 import { storiesOf } from "@storybook/react";
-import { withKnobs, number, boolean } from "@storybook/addon-knobs/react";
+import { text, number } from "@storybook/addon-knobs/react";
+
 import Input from "./Input";
 
-const stories = storiesOf("Input", module);
-
-stories.addDecorator(withKnobs);
-
-stories.add("standard", () => {
-  return (
-    <div style={{ width: "50vw" }}>
-      <Input
-        style={{
-          margin: "10px"
-        }}
-        placeholder="Default"
-        onChange={e => console.log("value changed", e)}
-        maxLength={number("maxLength", 100)}
-      />
-      <Input
-        shouldFitContainer={boolean("shouldFitContainer", true)}
-        style={{
-          margin: "10px"
-        }}
-        placeholder="shouldFitContainer"
-        onChange={e => console.log("value changed", e)}
-        maxLength={number("maxLength", 100)}
-      />
-    </div>
-  );
-});
+storiesOf("Input", module).add("default", () => (
+  <Input
+    placeholder={text("placeholder", "Placeholder")}
+    maxLength={number("maxLength", 25)}
+  />
+));
