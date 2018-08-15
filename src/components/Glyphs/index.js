7e5ef53f51314481f0ee0b3740b887372cd442c4
kaitmore <kaitmore@gmail.com>
2018-08-15 13:44:37 -0400

    fix: add all glyphs to export file

--- src/components/Glyphs/index.js
+++ src/components/Glyphs/index.js
@@ -1,111 +1,369 @@
+import ArrowDown from "./ArrowDown";
+import ArrowDownSmall from "./ArrowDownSmall";
+import ArrowLeft from "./ArrowLeft";
+import ArrowLeftSmall from "./ArrowLeftSmall";
+import ArrowRight from "./ArrowRight";
+import ArrowRightSmall from "./ArrowRightSmall";
+import ArrowUp from "./ArrowUp";
+import ArrowUpSmall from "./ArrowUpSmall";
 import Bars from "./Bars";
 import Bell from "./Bell";
+import Bullet from "./Bullet";
 import CPU from "./CPU";
+import Calendar from "./Calendar";
 import Card from "./Card";
+import Check from "./Check";
+import Checklist from "./Checklist";
+import ChevronDown from "./ChevronDown";
+import ChevronDownSmall from "./ChevronDownSmall";
+import ChevronLeft from "./ChevronLeft";
+import ChevronLeftSmall from "./ChevronLeftSmall";
+import ChevronRight from "./ChevronRight";
+import ChevronRightSmall from "./ChevronRightSmall";
+import ChevronUp from "./ChevronUp";
+import ChevronUpSmall from "./ChevronUpSmall";
+import Child from "./Child";
+import CircleThick from "./CircleThick";
+import CircleThin from "./CircleThin";
 import Close from "./Close";
+import Cloud from "./Cloud";
 import Cog from "./Cog";
+import Collapse from "./Collapse";
+import CommentAdd from "./CommentAdd";
+import Confidential from "./Confidential";
 import Configuration from "./Configuration";
+import Cycle from "./Cycle";
+import Dashboards from "./Dashboards";
 import Docs from "./Docs";
+import Drive from "./Drive";
 import EKG from "./EKG";
+import EKGMagnifier from "./EKGMagnifier";
 import EditGraph from "./EditGraph";
+import EllipsisHorizontal from "./EllipsisHorizontal";
+import EllipsisVertical from "./EllipsisVertical";
+import Entity from "./Entity";
 import ErrorList from "./ErrorList";
 import Exclamation from "./Exclamation";
 import Explorer from "./Explorer";
+import Eye from "./Eye";
 import Fabric from "./Fabric";
+import File from "./File";
+import FileAudio from "./FileAudio";
+import FileDocs from "./FileDocs";
+import FileDocument from "./FileDocument";
+import FileDownload from "./FileDownload";
+import FileFind from "./FileFind";
+import FileGhost from "./FileGhost";
+import FilePhoto from "./FilePhoto";
+import FilePresentation from "./FilePresentation";
+import FileSharedWithMe from "./FileSharedWithMe";
+import FileSharedWithOthers from "./FileSharedWithOthers";
+import FileSpreadsheet from "./FileSpreadsheet";
+import FileStar from "./FileStar";
+import FileText from "./FileText";
+import FileVideo from "./FileVideo";
+import Files from "./Files";
+import FilesFind from "./FilesFind";
+import FilesSharedWithMe from "./FilesSharedWithMe";
+import FilesSharedWithOthers from "./FilesSharedWithOthers";
 import Finagle from "./Finagle";
+import Folder from "./Folder";
+import FolderAdd from "./FolderAdd";
+import FolderGroup from "./FolderGroup";
+import FolderShared from "./FolderShared";
 import Functions from "./Functions";
 import GRPC from "./GRPC";
 import GitHub from "./GitHub";
+import Globe from "./Globe";
+import HTTP2 from "./HTTP2";
 import Http from "./Http";
+import HttpDelete from "./HttpDelete";
+import HttpGet from "./HttpGet";
+import HttpPatch from "./HttpPatch";
+import HttpPost from "./HttpPost";
+import HttpPut from "./HttpPut";
+import Idle from "./Idle";
 import Info from "./Info";
+import IngressEgress from "./IngressEgress";
 import Instances from "./Instances";
+import ItemsFilter from "./ItemsFilter";
+import ItemsGrid from "./ItemsGrid";
+import ItemsGroup from "./ItemsGroup";
+import ItemsRows from "./ItemsRows";
+import ItemsSort from "./ItemsSort";
 import JVM from "./JVM";
 import Key from "./Key";
+import Latency from "./Latency";
+import LinkOut from "./LinkOut";
 import LinkedIn from "./LinkedIn";
 import List from "./List";
+import Lock from "./Lock";
+import Magnifier from "./Magnifier";
 import Memory from "./Memory";
+import Minus from "./Minus";
+import MinusSmall from "./MinusSmall";
+import MinusXSmall from "./MinusXSmall";
+import NLP from "./NLP";
 import Negation from "./Negation";
+import NoEye from "./NoEye";
 import NoKey from "./NoKey";
+import NoLock from "./NoLock";
 import NoMetrics from "./NoMetrics";
+import Object from "./Object";
+import ObjectAdd from "./ObjectAdd";
+import ObjectAudio from "./ObjectAudio";
+import ObjectDocument from "./ObjectDocument";
+import ObjectDownload from "./ObjectDownload ";
+import ObjectDuplicate from "./ObjectDuplicate";
+import ObjectExpand from "./ObjectExpand";
+import ObjectGhost from "./ObjectGhost";
+import ObjectMail from "./ObjectMail";
+import ObjectMove from "./ObjectMove";
+import ObjectPhoto from "./ObjectPhoto";
+import ObjectPlay from "./ObjectPlay";
+import ObjectPresentation from "./ObjectPresentation";
+import ObjectSpreadsheet from "./ObjectSpreadsheet";
+import ObjectStar from "./ObjectStar";
+import ObjectText from "./ObjectText";
+import ObjectUpload from "./ObjectUpload";
+import ObjectVideo from "./ObjectVideo";
+import ObjectsUpload from "./ObjectsUpload";
+import Observation from "./Observation";
+import OnOff from "./OnOff";
+import Parent from "./Parent";
 import Pause from "./Pause";
+import Paused from "./Paused";
+import Pencil from "./Pencil";
+import PencilGraph from "./PencilGraph";
+import PencilParagraph from "./PencilParagraph";
 import Person from "./Person";
 import Play from "./Play";
+import Plus from "./Plus";
+import PlusSmall from "./PlusSmall";
+import PlusXSmall from "./PlusXSmall";
 import Poll from "./Poll";
 import Power from "./Power";
 import Rows from "./Rows";
+import Running from "./Running";
 import RunningSmall from "./RunningSmall";
 import Scale from "./Scale";
 import Scatterplot from "./Scatterplot";
+import Secret from "./Secret";
 import Service from "./Service";
 import ServiceInstance from "./ServiceInstance";
+import Services from "./Services";
 import ServicesWhite from "./ServicesWhite";
+import Shapes from "./Shapes";
+import SquareThick from "./SquareThick";
+import SquareThin from "./SquareThin";
+import Stack from "./Stack";
+import Star from "./Star";
 import StarFilled from "./StarFilled";
+import StepBackward from "./StepBackward";
+import StepForward from "./StepForward";
+import Stopped from "./Stopped";
 import Summary from "./Summary";
 import Tape from "./Tape";
 import Threads from "./Threads";
 import Timer from "./Timer";
+import TopSecret from "./TopSecret";
+import Trash from "./Trash";
+import TriangleThick from "./TriangleThick";
+import TriangleThin from "./TriangleThin";
 import Twitter from "./Twitter";
+import Unclassified from "./Unclassified";
+import Unlock from "./Unlock";
+import Upload from "./Upload";
+import UserGroup from "./UserGroup";
+import UserRound from "./UserRound";
+import UserShare from "./UserShare";
 import ViewCollapse from "./ViewCollapse";
-import HttpGet from "./HttpGet";
-import HttpPut from "./HttpPut";
-import HttpPost from "./HttpPost";
-import HttpPatch from "./HttpPatch";
-import HttpDelete from "./HttpDelete";
-import ArrowDown from "./ArrowDown";
+import Warning from "./Warning";
+import X from "./X";
+import XSmall from "./XSmall";
+
 export {
   ArrowDown,
+  ArrowDownSmall,
+  ArrowLeft,
+  ArrowLeftSmall,
+  ArrowRight,
+  ArrowRightSmall,
+  ArrowUp,
+  ArrowUpSmall,
   Bars,
   Bell,
+  Bullet,
   CPU,
+  Calendar,
   Card,
+  Check,
+  Checklist,
+  ChevronDown,
+  ChevronDownSmall,
+  ChevronLeft,
+  ChevronLeftSmall,
+  ChevronRight,
+  ChevronRightSmall,
+  ChevronUp,
+  ChevronUpSmall,
+  Child,
+  CircleThick,
+  CircleThin,
   Close,
+  Cloud,
   Cog,
+  Collapse,
+  CommentAdd,
+  Confidential,
   Configuration,
-  HttpDelete,
+  Cycle,
+  Dashboards,
   Docs,
+  Drive,
   EKG,
+  EKGMagnifier,
   EditGraph,
+  EllipsisHorizontal,
+  EllipsisVertical,
+  Entity,
   ErrorList,
   Exclamation,
   Explorer,
+  Eye,
   Fabric,
+  File,
+  FileAudio,
+  FileDocs,
+  FileDocument,
+  FileDownload,
+  FileFind,
+  FileGhost,
+  FilePhoto,
+  FilePresentation,
+  FileSharedWithMe,
+  FileSharedWithOthers,
+  FileSpreadsheet,
+  FileStar,
+  FileText,
+  FileVideo,
+  Files,
+  FilesFind,
+  FilesSharedWithMe,
+  FilesSharedWithOthers,
   Finagle,
+  Folder,
+  FolderAdd,
+  FolderGroup,
+  FolderShared,
   Functions,
-  HttpGet,
   GRPC,
   GitHub,
-  JVM as Go,
+  Globe,
+  HTTP2,
   Http,
-  Info,
-  Instances,
-  JVM,
-  Key,
-  LinkedIn,
-  List,
-  Memory,
-  Negation,
-  NoKey,
-  NoMetrics,
+  HttpDelete,
+  HttpGet,
   HttpPatch,
   HttpPost,
   HttpPut,
+  Idle,
+  Info,
+  IngressEgress,
+  Instances,
+  ItemsFilter,
+  ItemsGrid,
+  ItemsGroup,
+  ItemsRows,
+  ItemsSort,
+  JVM,
+  Key,
+  Latency,
+  LinkOut,
+  LinkedIn,
+  List,
+  Lock,
+  Magnifier,
+  Memory,
+  Minus,
+  MinusSmall,
+  MinusXSmall,
+  NLP,
+  Negation,
+  NoEye,
+  NoKey,
+  NoLock,
+  NoMetrics,
+  Object,
+  ObjectAdd,
+  ObjectAudio,
+  ObjectDocument,
+  ObjectDownload,
+  ObjectDuplicate,
+  ObjectExpand,
+  ObjectGhost,
+  ObjectMail,
+  ObjectMove,
+  ObjectPhoto,
+  ObjectPlay,
+  ObjectPresentation,
+  ObjectSpreadsheet,
+  ObjectStar,
+  ObjectText,
+  ObjectUpload,
+  ObjectVideo,
+  ObjectsUpload,
+  Observation,
+  OnOff,
+  Parent,
   Pause,
+  Paused,
+  Pencil,
+  PencilGraph,
+  PencilParagraph,
   Person,
   Play,
+  Plus,
+  PlusSmall,
+  PlusXSmall,
   Poll,
   Power,
   Rows,
+  Running,
   RunningSmall,
   Scale,
   Scatterplot,
+  Secret,
   Service,
   ServiceInstance,
+  Services,
   ServicesWhite,
+  Shapes,
+  SquareThick,
+  SquareThin,
+  Stack,
+  Star,
   StarFilled,
+  StepBackward,
+  StepForward,
+  Stopped,
   Summary,
   Tape,
   Threads,
   Timer,
+  TopSecret,
+  Trash,
+  TriangleThick,
+  TriangleThin,
   Twitter,
-  ViewCollapse
+  Unclassified,
+  Unlock,
+  Upload,
+  UserGroup,
+  UserRound,
+  UserShare,
+  ViewCollapse,
+  Warning,
+  X,
+  XSmall
 };
