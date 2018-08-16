ea67689d0b283dd4a34a31a13a96cd595a168657
Kait Moreno <kaitlin.moreno@deciphernow.com>
2018-08-16 17:07:51 -0400

    feat: new icons (#250)
    * feat: add new icons
    
    * fix: react import
    
    * fix: remove fill from all icons
    
    * fix: file audio
    
    * fix: add all glyphs to export file
    
    * fix: update react import
    
    * fix: storybook
    
    * fix: remove old tests, fix various icons
    
    * fix: remove readdir
    
    * fix: rename HTTP
    
    * feat: update icons
    
    * test: update snaps

---
+++ src/components/Glyphs/IconArrowLeft.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Icon from "../Icon";
+
+export default function ArrowLeft(props) {
+  return (
+    <Icon {...props} glyphName="ArrowLeft">
+      <polygon
+        id="Mask"
+         
+        fillRule="nonzero"
+        transform="translate(11.500000, 12.000000) scale(-1, 1) translate(-11.500000, -12.000000) "
+        points="8.99999999 7.99999998 14 12 8.99999999 16"
+      />
+    </Icon>
+  );
+}
+
+ArrowLeft.displayName = "ArrowLeft";
