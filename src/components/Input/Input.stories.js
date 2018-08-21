049d9887d03760aee4f3e907c9666de2ff879f9b
shanberg <shanberg@users.noreply.github.com>
2018-08-21 14:29:55 -0400

    fix: properly including knobs for storybook control

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { text, number, boolean } from "@storybook/addon-knobs/react";
+import { text, number, boolean, withKnobs } from "@storybook/addon-knobs/react";
 
 import Input from "./Input";
 
-storiesOf("Input", module).add("default", () => (
+storiesOf("Input", module)
+  .addDecorator(withKnobs)
+  .add("default", () => (
   <Input
     autofocus={boolean("Autofocus", false)}
     hint={text("Hint", "Hint")}
