bd979a5daeea0a7beaf465d5d2e3360dccaccc38
shanberg <shanberg@users.noreply.github.com>
2018-08-23 14:04:56 -0400

    fix: adding nominal tests and shapshots to input components

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styled from 'styled-components';
-import {
-  FONT_SIZE_BASE,
-  FONT_SIZE_XS,
-  FONT_STACK_BASE,
-  COLOR_KEYLINE,
-  COLOR_CONTENT,
-  BORDER_RADIUS_BASE,
-  COLOR_HIGHLIGHT
-} from 'style/styleVariables';
-import { spacingScale } from 'style/styleFunctions';
+import InputLabel from './components/InputLabel';
+import InputWrap from './components/InputWrap';
+import InputHint from './components/InputHint';
+import InputField from './components/InputField';
 
 
 Input.propTypes = {
@@ -25,58 +18,6 @@ Input.propTypes = {
     'text',
   ]),
 };
-
-
-const InputWrap = styled.label`
-  font-family: ${FONT_STACK_BASE};
-  position: relative;
-`;
-
-const InputLabel = styled.p`
-  margin: ${spacingScale(0.5)} 0;
-  font-size: ${FONT_SIZE_BASE};
-`;
-
-const InputHint = styled.small`
-  margin: ${spacingScale(0.5)} 0;
-  font-size: ${FONT_SIZE_XS};
-  opacity: 0.5;
-  display: block;
-`;
-
-
-const InputField = styled.input.attrs({
-  autoFocus: props => props.autofocus,
-  defaultValue: props => props.defaultValue,
-  placeholder: props => props.placeholder,
-  maxLength: props => props.maxlength,
-  type: props => props.type,
-})`
-  padding: ${spacingScale(0.5) + ' ' + spacingScale(1)};
-  height: ${spacingScale(4)};
-  border: 1px solid ${COLOR_KEYLINE};
-  border-radius: ${BORDER_RADIUS_BASE};
-  color: ${COLOR_CONTENT};
-  font-family: ${FONT_STACK_BASE};
-  font-size: ${FONT_SIZE_BASE};
-  background: transparent;
-  box-sizing: border-box;
-  line-height: 1.5;
-  transition: all 0.15s ease;
-  user-select: auto;
-  flex: 1 1 auto;
-
-  &::placeholder {
-    color: ${COLOR_CONTENT};
-    transition: inherit;
-    opacity: 0.5;
-  }
-
-  &:focus {
-    border-color: ${COLOR_HIGHLIGHT};
-    outline: none;
-  }
-`;
 
 
 export default function Input({ autofocus, defaultValue, hint, label, maxLength, placeholder, type, ...props }) {
---
+++ src/components/Input/components/InputField.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+import { FONT_SIZE_BASE, FONT_STACK_BASE, COLOR_KEYLINE, COLOR_CONTENT, BORDER_RADIUS_BASE, COLOR_HIGHLIGHT } from 'style/styleVariables';
+import { spacingScale } from 'style/styleFunctions';
+
+export const InputField = styled.input.attrs({
+  autoFocus: props => props.autofocus,
+  defaultValue: props => props.defaultValue,
+  placeholder: props => props.placeholder,
+  maxLength: props => props.maxlength,
+  type: props => props.type,
+})`
+  padding: ${spacingScale(0.5) + ' ' + spacingScale(1)};
+  height: ${spacingScale(4)};
+  border: 1px solid ${COLOR_KEYLINE};
+  border-radius: ${BORDER_RADIUS_BASE};
+  color: ${COLOR_CONTENT};
+  font-family: ${FONT_STACK_BASE};
+  font-size: ${FONT_SIZE_BASE};
+  background: transparent;
+  box-sizing: border-box;
+  line-height: 1.5;
+  transition: all 0.15s ease;
+  user-select: auto;
+  flex: 1 1 auto;
+
+  &::placeholder {
+    color: ${COLOR_CONTENT};
+    transition: inherit;
+    opacity: 0.5;
+  }
+
+  &:focus {
+    border-color: ${COLOR_HIGHLIGHT};
+    outline: none;
+  }
+`;
+
+export default InputField;
