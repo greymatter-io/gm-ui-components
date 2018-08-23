9784c2c2bb9a3df295aa56715aef42f207973ff7
shanberg <shanberg@users.noreply.github.com>
2018-08-23 14:25:09 -0400

    fix: removing dead code

--- src/style/styleVariables.js
+++ src/style/styleVariables.js
@@ -1,5 +1,5 @@
 
-/*/ Colors /*/
+// COLORS
 export const COLOR_BRAND_A = "#0aab2a";
 export const COLOR_BRAND_B = "#002e6e";
 export const COLOR_CONTENT = '#000';
@@ -9,7 +9,6 @@ export const COLOR_BACKGROUND_B = '#F8F8F8';
 export const COLOR_BACKGROUND_C = '#000';
 
 export const COLOR_INTENT_HIGHLIGHT = COLOR_BRAND_A;
-// export const COLOR_INTENT_HIGHLIGHT = '#068920';
 export const COLOR_INTENT_SUCCESS = COLOR_BRAND_A;
 export const COLOR_INTENT_DANGER = "#D83D22"; // WCAG AA+
 export const COLOR_INTENT_WARNING = "#F7CD45"; // WCAG AA+
@@ -23,13 +22,10 @@ export const OPACITY_70 = '0.7';
 export const OPACITY_50 = '0.5';
 export const OPACITY_15 = '0.15';
 
-// DEPRECATE
-export const DARK_ON_LIGHT_CONTRAST_ENHANCEMENT_RATIO = 2;
-
 // LAYOUT
-export const BORDER_RADIUS_BASE = "3px"; // 4px
+export const BORDER_RADIUS_BASE = "3px"; // Will be changed to 4px
 export const SPACING_BASE = 8; 
-export const ICON_VIEWBOX_SIZE = SPACING_BASE * 3; // Move to Icons component? spacingScale(3)?
+export const ICON_VIEWBOX_SIZE = SPACING_BASE * 3; // Move to Icons component?
 
 export const ZINDEX_TOOLTIP = "1070";
 export const ZINDEX_STICKY = "1010";
@@ -43,15 +39,19 @@ export const FONT_STACK_BASE = `${FONTS_SYSTEM}`;
 export const FONT_STACK_BRAND = `${FONTS_BRAND}, ${FONTS_SYSTEM}`;
 export const FONT_STACK_CODE = `${FONTS_CODE}, ${FONTS_SYSTEM}`;
 
-export const FONT_SIZE_HERO = "40px"; // GOOD
-export const FONT_SIZE_TITLE = "36px"; // 32px
-export const FONT_SIZE_LG = "18px"; // 24px
-export const FONT_SIZE_MD = "16px"; // 18px
-export const FONT_SIZE_BASE = "14px"; // GOOD
-export const FONT_SIZE_SM = "11px"; // 12px
-export const FONT_SIZE_XS = "9px"; // 10px
+export const FONT_SIZE_HERO = "40px";
+export const FONT_SIZE_TITLE = "36px"; // Will be changed to 32px
+export const FONT_SIZE_LG = "18px"; // Will be changed to 24px
+export const FONT_SIZE_MD = "16px"; // Will be changed to 18px
+export const FONT_SIZE_BASE = "14px";
+export const FONT_SIZE_SM = "11px"; // Will be changed to 12px
+export const FONT_SIZE_XS = "9px"; // Will be changed to 10px
 
 export const LINE_HEIGHT_BASE = 1.4;
 export const FONT_WEIGHT_BASE = 400;
-export const FONT_WEIGHT_SEMIBOLD = FONT_WEIGHT_BASE * 1.25;
-export const FONT_WEIGHT_BOLD = 600; // 700? 800?
+export const FONT_WEIGHT_SEMIBOLD = FONT_WEIGHT_BASE * 1.25; // Will be changed to 600
+export const FONT_WEIGHT_BOLD = 600; // Will be changed to 700 or 800
+
+
+// DEPRECATE
+export const DARK_ON_LIGHT_CONTRAST_ENHANCEMENT_RATIO = 2;
