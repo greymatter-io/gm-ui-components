232cb203651171fcf1656d69c9cbbc46ca7daabd
shanberg <shanberg@users.noreply.github.com>
2018-09-03 09:46:26 -0400

    feat: add viewport addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,3 +2,5 @@ import "@storybook/addon-links/register";
 import "@storybook/addon-knobs/register";
 import "@storybook/addon-a11y/register";
 import "@storybook/addon-options/register";
+import '@storybook/addon-viewport/register'
+
