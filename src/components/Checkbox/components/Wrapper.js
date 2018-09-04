da4e845e2ddf1460ef4856ca9e25d21c6eedcc06
shanberg <shanberg@users.noreply.github.com>
2018-09-04 08:55:17 -0400

    feat: proofing themeing concept with checkbox

--- src/components/Checkbox/components/Wrapper.js
+++ src/components/Checkbox/components/Wrapper.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
-import { FONT_STACK_BASE, FONT_SIZE_BASE } from "style/styleVariables";
+import { COLOR_CONTENT, FONT_STACK_BASE, FONT_SIZE_BASE } from "style/styleVariables";
 import { spacingScale } from "style/styleFunctions";
+
+const fontFamily = props => props.theme.FONT_STACK_BASE || FONT_STACK_BASE;
+const fontSize = props => props.theme.FONT_SIZE_BASE || FONT_SIZE_BASE;
+const color = props => props.theme.COLOR_CONTENT || COLOR_CONTENT;
 
 const Wrapper = styled.label`
   ${props => getPosition(props.labelPosition)};
-  font-family: ${FONT_STACK_BASE};
-  font-size: ${FONT_SIZE_BASE};
+  font-family: ${fontFamily};
+  font-size: ${fontSize};
+  color: ${color};
   display: flex;
   width: fit-content;
   align-items: center;
