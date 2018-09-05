e880989070c591ac3e0ccdbcec7d6093893776bf
kaitmore <kaitmore@gmail.com>
2018-09-05 18:08:56 -0400

    test: update tests

--- src/components/Radio/Radio.test.js
+++ src/components/Radio/Radio.test.js
@@ -1,11 +1,22 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 import Radio from "./Radio";
 
 describe("Radio", () => {
   it("should render", () => {
-    const aRadio = renderer.create(<Radio label="label" />).toJSON();
+    const aRadio = shallow(<Radio label="label" />).dive();
     expect(aRadio).toMatchSnapshot();
+  });
+
+  it("should different positions", () => {
+    const positions = ["left", "right", "top", "bottom"];
+
+    positions.forEach(position => {
+      const aRadio = shallow(
+        <Radio label="label" position={position} />
+      ).dive();
+      expect(aRadio).toMatchSnapshot();
+    });
   });
 });
