b093e2c58cf10fdf1306a7f4fa4e1636cc95262a
shanberg <shanberg@users.noreply.github.com>
2018-09-10 08:10:54 -0400

    fix: hooking up new components in the index

--- src/components/index.js
+++ src/components/index.js
@@ -3,21 +3,31 @@ import { Breadcrumbs, BreadcrumbItem } from "./Breadcrumbs";
 import Button from "./Button";
 import ButtonGroup from "./ButtonGroup";
 import { Checkbox } from "./Form/scenes/Checkbox";
+import AppHeader from "./AppHeader";
+import ErrorCard from "./ErrorCard";
 import Icon from "./Icon";
-import InputText from "./InputText";
+import Input from "./Form/scenes/Input";
+import InputEmail from "./Form/scenes/InputEmail";
+import InputNumber from "./Form/scenes/InputNumber";
+import InputPassword from "./Form/scenes/InputPassword";
+import InputRange from "./Form/scenes/InputRange";
+import InputSearch from "./Form/scenes/InputSearch";
+import InputTelephone from "./Form/scenes/InputTelephone";
+import InputText from "./Form/scenes/InputText";
+import InputUrl from "./Form/scenes/InputUrl";
+import LayoutSection from "./LayoutSection";
+import NavCard from "./NavCard";
+import NavCardGroup from "./NavCardGroup";
+import Radio from "./Form/scenes/Radio";
+import Readout from "./Readout";
+import ReadoutGroup from "./ReadoutGroup";
+import Select from "./Form/scenes/Select";
 import Spinner from "./Spinner";
 import Tab from "./Tab";
 import TabGroup from "./TabGroup";
 import Table from "./Table";
+import Textarea from "./Form/scenes/Textarea";
 import Tooltip from "./Tooltip";
-import Readout from "./Readout";
-import ReadoutGroup from "./ReadoutGroup";
-import LayoutSection from "./LayoutSection";
-import AppHeader from "./AppHeader";
-import ErrorCard from "./ErrorCard";
-import NavCard from "./NavCard";
-import NavCardGroup from "./NavCardGroup";
-import Radio from "./Radio";
 import * as Glyphs from "./Glyphs";
 
 const library = {
@@ -31,17 +41,27 @@ const library = {
   Checkbox,
   ErrorCard,
   Icon,
+  Input,
+  InputEmail,
+  InputNumber,
+  InputPassword,
+  InputRange,
+  InputSearch,
+  InputTelephone,
   InputText,
+  InputUrl,
   LayoutSection,
   NavCard,
   NavCardGroup,
   Radio,
   Readout,
   ReadoutGroup,
+  Select,
   Spinner,
   Tab,
   TabGroup,
   Table,
+  Textarea,
   Tooltip
 };
 
