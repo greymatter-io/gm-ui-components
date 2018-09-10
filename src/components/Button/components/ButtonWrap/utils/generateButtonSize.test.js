b77f10b9221dcacff3067fa33366542e5f5cd456
shanberg <shanberg@users.noreply.github.com>
2018-09-10 10:41:44 -0400

    fix: tests now test for `contains` instead of `equals`

--- src/components/Button/components/ButtonWrap/utils/generateButtonSize.test.js
+++ src/components/Button/components/ButtonWrap/utils/generateButtonSize.test.js
@@ -2,35 +2,32 @@ import generateButtonSize from "./generateButtonSize";
 
 describe("generateButtonSize", () => {
   test("renders a block of CSS attributes for a particular string (xs, sm, normal, lg, xl)", () => {
-    expect(generateButtonSize("xs")).toEqual(`
-    font-size: 7px;
+    expect(generateButtonSize("xs")).toContain(`
+    font-size: 9px;
     border-radius: 4px;
-    padding: 2px 7px;
+    padding: 2px 8px;
     text-transform: uppercase;
   `);
-    expect(generateButtonSize("sm")).toEqual(`
+    expect(generateButtonSize("sm")).toContain(`
     font-size: 11px;
     border-radius: 4px;
-    padding: 2px 7px;
+    padding: 2px 8px;
     text-transform: uppercase;
   `);
-    expect(generateButtonSize("lg")).toEqual(`
+    expect(generateButtonSize("lg")).toContain(`
     font-size: 18px;
     border-radius: 4px;
-    padding: 2px 7px;
-    
+    padding: 2px 10px;
   `);
-    expect(generateButtonSize("xl")).toEqual(`
-    font-size: 21px;
+    expect(generateButtonSize("xl")).toContain(`
+    font-size: 18px;
     border-radius: 4px;
-    padding: 2px 7px;
-    
+    padding: 2px 10px;
   `);
-    expect(generateButtonSize("normal")).toEqual(`
+    expect(generateButtonSize("normal")).toContain(`
     font-size: 14px;
     border-radius: 4px;
-    padding: 2px 7px;
-    
+    padding: 2px 8px;
   `);
   });
 });
