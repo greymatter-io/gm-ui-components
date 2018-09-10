7e72fb421495203712134fa4ef31ab54e967ef15
shanberg <shanberg@users.noreply.github.com>
2018-09-10 09:17:10 -0400

    feat: cleaner more consistent button sizing and radius

--- src/components/Button/components/ButtonWrap/utils/generateButtonSize.test.js
+++ src/components/Button/components/ButtonWrap/utils/generateButtonSize.test.js
@@ -4,31 +4,31 @@ describe("generateButtonSize", () => {
   test("renders a block of CSS attributes for a particular string (xs, sm, normal, lg, xl)", () => {
     expect(generateButtonSize("xs")).toEqual(`
     font-size: 7px;
-    border-radius: 3px;
+    border-radius: 4px;
     padding: 2px 7px;
     text-transform: uppercase;
   `);
     expect(generateButtonSize("sm")).toEqual(`
     font-size: 11px;
-    border-radius: 3px;
+    border-radius: 4px;
     padding: 2px 7px;
     text-transform: uppercase;
   `);
     expect(generateButtonSize("lg")).toEqual(`
     font-size: 18px;
-    border-radius: 3px;
+    border-radius: 4px;
     padding: 2px 7px;
     
   `);
     expect(generateButtonSize("xl")).toEqual(`
     font-size: 21px;
-    border-radius: 3px;
+    border-radius: 4px;
     padding: 2px 7px;
     
   `);
     expect(generateButtonSize("normal")).toEqual(`
     font-size: 14px;
-    border-radius: 3px;
+    border-radius: 4px;
     padding: 2px 7px;
     
   `);
