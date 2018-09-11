12ef2f6f1169005e22a4c57dccc4a1c9df5ce0ee
shanberg <shanberg@users.noreply.github.com>
2018-09-11 09:52:24 -0400

    feat: theme layoutsection

--- src/components/LayoutSection/components/LayoutSectionWrap.js
+++ src/components/LayoutSection/components/LayoutSectionWrap.js
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 
 import { spacingScale } from "style/styleFunctions";
-import { COLOR_CONTENT, FONT_STACK_BASE } from "style/styleVariables";
 
 const LayoutSectionWrap = styled.section`
+  color: ${props => props.theme.COLOR_CONTENT};
+  font-family: ${props => props.theme.FONT_STACK_BASE};
   align-items: stretch;
-  color: ${COLOR_CONTENT};
   display: flex;
   flex: 0 0 auto;
   flex-direction: column;
   padding: ${spacingScale(1)};
-  font-family: ${FONT_STACK_BASE};
 
   &:not(:first-of-type) {
     margin-top: ${spacingScale(4)};
