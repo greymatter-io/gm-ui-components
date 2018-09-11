0b4ffc6ac0c79f96e3d78ea06d132e70cc67349f
shanberg <shanberg@users.noreply.github.com>
2018-09-11 09:52:24 -0400

    feat: theme layoutsection

--- src/components/LayoutSection/LayoutSection.stories.js
+++ src/components/LayoutSection/LayoutSection.stories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { storiesOf } from "@storybook/react";
 import { text } from "@storybook/addon-knobs";
 import { withInfo } from "@storybook/addon-info";
@@ -6,43 +7,49 @@ import { withInfo } from "@storybook/addon-info";
 import LayoutSection from "./LayoutSection";
 import { Readout, ReadoutGroup, IconBell, IconSummary } from "../";
 
+const ThemedBackground = styled.div`
+  background-color: ${props => props.theme.COLOR_BACKGROUND_A};
+`;
+
 storiesOf("Layout Section", module).add(
   "default",
   withInfo("An flexbox layout component")(() => {
     return (
-      <LayoutSection
-        title={text("title", "Test Layout")}
-        icon={() => <IconBell size={"24px"} />}
-      >
-        <ReadoutGroup>
-          <Readout
-            readoutItems={[
-              {
-                title: "Uptime",
-                value: "1000ms"
-              }
-            ]}
-          />
-          <Readout
-            primary
-            readoutItems={[
-              {
-                title: "Error Rate",
-                value: "0.000%",
-                icon: () => <IconSummary size={"24px"} />
-              }
-            ]}
-          />
-          <Readout
-            readoutItems={[
-              {
-                title: "Host CPU Cores",
-                value: "0"
-              }
-            ]}
-          />
-        </ReadoutGroup>
-      </LayoutSection>
+      <ThemedBackground>
+        <LayoutSection
+          title={text("title", "Test Layout")}
+          icon={() => <IconBell size={"24px"} />}
+        >
+          <ReadoutGroup>
+            <Readout
+              readoutItems={[
+                {
+                  title: "Uptime",
+                  value: "1000ms"
+                }
+              ]}
+            />
+            <Readout
+              primary
+              readoutItems={[
+                {
+                  title: "Error Rate",
+                  value: "0.000%",
+                  icon: () => <IconSummary size={"24px"} />
+                }
+              ]}
+            />
+            <Readout
+              readoutItems={[
+                {
+                  title: "Host CPU Cores",
+                  value: "0"
+                }
+              ]}
+            />
+          </ReadoutGroup>
+        </LayoutSection>
+      </ThemedBackground>
     );
   })
 );
