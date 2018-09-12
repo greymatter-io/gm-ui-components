eccd9699cbbb6472557a39f193855dbbe90100ad
shanberg <shanberg@users.noreply.github.com>
2018-09-12 07:53:30 -0400

    fix: fixing tooltip render

--- src/components/Tooltip/components/TooltipWrap.js
+++ src/components/Tooltip/components/TooltipWrap.js
@@ -4,13 +4,14 @@ const TooltipWrap = styled.div`
   display: inline-block;
   position: relative;
   font-family: ${props => props.theme.FONT_STACK_BASE};
+
   :hover > :last-child {
     ${props =>
       !props.hideTooltip &&
       css`
         opacity: 1;
         visibility: visible;
-        z-index: ${props => props.theme.ZINDEX_TOOLTIP};`};
+        z-index: ${props => props.theme.ZINDEX_TOOLTIP};
       `};
   }
 `;
