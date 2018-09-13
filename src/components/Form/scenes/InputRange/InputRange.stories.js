9aefee0fe6478c2aa238a5f3b6986111b4c2e5c7
shanberg <shanberg@users.noreply.github.com>
2018-09-13 15:26:03 -0400

    fix: removing unused input story

--- src/components/Form/scenes/InputRange/InputRange.stories.js
+++ src/components/Form/scenes/InputRange/InputRange.stories.js
@@ -20,36 +20,3 @@ stories.add(
     );
   })
 );
-
-stories.add(
-  "ticks",
-  withInfo("A range input component with label, optional hint, and tickmarks.")(
-    () => {
-      return (
-        <div>
-          <InputRange
-            autoFocus={boolean("autoFocus", false)}
-            hint={text("hint", "hint")}
-            list="tickmarks"
-            label={text("label", "label")}
-            defaultValue={text("defaultValue", "defaultValue")}
-          />
-
-          <datalist id="tickmarks">
-            <option value="0" label="0%" />
-            <option value="10" />
-            <option value="20" />
-            <option value="30" />
-            <option value="40" />
-            <option value="50" label="50%" />
-            <option value="60" />
-            <option value="70" />
-            <option value="80" />
-            <option value="90" />
-            <option value="100" label="100%" />
-          </datalist>
-        </div>
-      );
-    }
-  )
-);
