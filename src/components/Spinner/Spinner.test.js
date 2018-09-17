1b856189d4afab08e006ca8a9a20aa3fe8abaad9
shanberg <shanberg@users.noreply.github.com>
2018-09-17 09:14:30 -0400

    test: adding more spinner tests

--- src/components/Spinner/Spinner.test.js
+++ src/components/Spinner/Spinner.test.js
@@ -2,9 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import Spinner from "./Spinner";
 
-describe("Spinner", () => {
+describe("Vertical Spinner", () => {
   it("matches snapshot", () => {
     const aSpinner = shallow(<Spinner orientation="vertical" />);
+    expect(aSpinner).toMatchSnapshot();
+  });
+});
+
+describe("Horizontal Spinner", () => {
+  it("matches snapshot", () => {
+    const aSpinner = shallow(<Spinner orientation="horizontal" />);
     expect(aSpinner).toMatchSnapshot();
   });
 });
