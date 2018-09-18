5c3c5d14ae3b9de377ac6c23d643d37c9c3fafaa
Stuart Hanberg <shanberg@users.noreply.github.com>
2018-09-18 10:05:32 -0400

    feat: minor improvements to radio button story (#356)

--- src/components/Form/scenes/Radio/Radio.stories.js
+++ src/components/Form/scenes/Radio/Radio.stories.js
@@ -2,52 +2,61 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { text, select } from "@storybook/addon-knobs/react";
+import { withInfo } from "@storybook/addon-info";
 import { Radio } from "components";
 
 const stories = storiesOf("Components|Radio", module);
 
-stories.add("standard", () => {
-  return (
-    <div style={{ width: "50vw" }}>
-      <Radio
-        labelPosition={select(
-          "labelPosition",
-          ["top", "bottom", "left", "right"],
-          "right"
-        )}
-        label={text("label", "this is a radio input")}
-      />
-    </div>
-  );
-});
+stories.add(
+  "standard",
+  withInfo("A general text input component with label and optional hint.")(
+    () => {
+      return (
+        <Radio
+          labelPosition={select(
+            "labelPosition",
+            ["top", "bottom", "left", "right"],
+            "right"
+          )}
+          label={text("label", "this is a radio input")}
+        />
+      );
+    }
+  )
+);
 
-stories.add("with props", () => {
-  return (
-    <div style={{ width: "50vw" }}>
-      <Radio
-        label={text(
-          "label",
-          "this is a checkbox defaulted to true with an onchange event handler"
-        )}
-        defaultChecked={true}
-        onChange={e => {
-          alert("checked: " + e.target.checked);
-        }}
-      />
-    </div>
-  );
-});
+stories.add(
+  "with props",
+  withInfo("A general text input component with label and optional hint.")(
+    () => {
+      return (
+        <Radio
+          label="this is a checkbox defaulted to false with an onchange event handler"
+          defaultChecked={false}
+          onChange={e => {
+            alert("checked: " + e.target.checked);
+          }}
+        />
+      );
+    }
+  )
+);
 
-stories.add("in a group", () => {
-  return (
-    <div style={{ width: "50vw" }}>
-      <Radio
-        label={text("label", "label")}
-        defaultChecked={true}
-        name="radioGroup"
-      />
-      <Radio label={text("label", "label")} name="radioGroup" />
-      <Radio label={text("label", "label")} name="radioGroup" />
-    </div>
-  );
-});
+stories.add(
+  "in a group",
+  withInfo("A general text input component with label and optional hint.")(
+    () => {
+      return (
+        <React.Fragment>
+          <Radio
+            label={text("label", "label")}
+            defaultChecked={true}
+            name="radioGroup"
+          />
+          <Radio label={text("label", "label")} name="radioGroup" />
+          <Radio label={text("label", "label")} name="radioGroup" />
+        </React.Fragment>
+      );
+    }
+  )
+);
