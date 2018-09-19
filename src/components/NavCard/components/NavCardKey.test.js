eaeaaee3f5d5a8fd9199aad9185d850535b51229
shanberg <shanberg@users.noreply.github.com>
2018-09-19 16:10:19 -0400

    fix: fixing tests

--- src/components/NavCard/components/NavCardKey.test.js
+++ src/components/NavCard/components/NavCardKey.test.js
@@ -2,9 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import NavCardKey from "./NavCardKey";
 
+const theme = {
+  OPACITY_15: "0.15",
+  OPACITY_70: "0.7",
+  COLOR_BACKGROUND_B: "blue",
+  COLOR_BACKGROUND_C: "red"
+};
+
 describe("NavCardKey", () => {
   it("matches snapshot", () => {
-    const aNavCardKey = shallow(<NavCardKey />);
+    const aNavCardKey = shallow(<NavCardKey theme={theme} />);
     expect(aNavCardKey).toMatchSnapshot();
   });
 });
