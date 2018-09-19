9561ffdae36454a2367afda2634ed73b73c4fb5f
shanberg <shanberg@users.noreply.github.com>
2018-09-19 14:06:07 -0400

    feat: theme loading spinner

--- src/components/Spinner/components/LoadingSpinner.js
+++ src/components/Spinner/components/LoadingSpinner.js
@@ -1,27 +1,34 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { transparentize } from "polished";
 
 import { spacingScale } from "style/styleFunctions";
 
 import spinGradient from "./spinGradient";
 
-export const LoadingSpinner = styled.div`
-  margin: ${spacingScale(1)};
-  border-radius: 100px;
+const backgroundImage = css`
   background-image: linear-gradient(
     to right,
     ${props => transparentize(1, props.theme.COLOR_BRAND_A)} 40%,
     ${props => props.theme.COLOR_BRAND_A} 60%,
     ${props => transparentize(1, props.theme.COLOR_BRAND_A)}
   );
+`;
+
+const pseudoElementShadow = css`
+  box-shadow: 0 0 1px ${props => transparentize(0.8, props.theme.COLOR_BRAND_A)};
+`;
+
+export const LoadingSpinner = styled.div`
+  margin: ${spacingScale(1)};
+  border-radius: 100px;
   background-size: 200% 200%;
   position: relative;
   animation: ${spinGradient} 16s ease infinite;
+  ${backgroundImage};
 
   &:after {
-    box-shadow: 0 0 1px
-      ${props => transparentize(0.8, props.theme.COLOR_BRAND_A)};
     background-color: ${props => props.theme.COLOR_BACKGROUND_A};
+    ${pseudoElementShadow};
     left: 1px;
     top: 1px;
     right: 1px;
