70efef804c19079b7b1bf9117a4483425300f618
shanberg <shanberg@users.noreply.github.com>
2018-09-19 15:52:35 -0400

    fix: fixing tests

--- src/components/Spinner/components/LoadingSpinner.test.js
+++ src/components/Spinner/components/LoadingSpinner.test.js
@@ -1,11 +1,16 @@
 import React from "react";
- import { shallow } from "enzyme";
+import { shallow } from "enzyme";
 
- import LoadingSpinner from "./LoadingSpinner";
+import LoadingSpinner from "./LoadingSpinner";
 
- describe("LoadingSpinner", () => {
-   it("matches snapshot", () => {
-     const aLoadingSpinner = shallow(<LoadingSpinner/>);
-     expect(aLoadingSpinner).toMatchSnapshot();
-   });
- });
+const theme = {
+  COLOR_BRAND_A: "red",
+  COLOR_BACKGROUND_A: "blue"
+};
+
+describe("LoadingSpinner", () => {
+  it("matches snapshot", () => {
+    const aLoadingSpinner = shallow(<LoadingSpinner theme={theme} />);
+    expect(aLoadingSpinner).toMatchSnapshot();
+  });
+});
