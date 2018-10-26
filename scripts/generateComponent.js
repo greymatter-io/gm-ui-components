988de3021caf066d0a82fed2bc88fc598cd3553e
kaitmore <kaitmore@gmail.com>
2018-10-26 09:50:41 -0400

    fix: catch node errors

--- scripts/generateComponent.js
+++ scripts/generateComponent.js
@@ -12,81 +12,89 @@ const {
   testTemplate
 } = require("../config/componentTemplates");
 
-if (process.argv.length <= 2) {
-  console.log("Please specify a component name");
-  process.exit(-1);
-}
-
-const fileName = process.argv[2];
-
-const PATH_PREFIX = `src/components/${fileName}`;
-
-// Create the directories if the component name doesn't already exist
-if (!fs.existsSync(`src/components/${fileName}`)) {
-  try {
-    fs.mkdirSync(`src/components/${fileName}`);
-    fs.mkdirSync(`src/components/${fileName}/components`);
-  } catch (err) {
-    throw new Error(err);
+try {
+  if (process.argv.length <= 2) {
+    throw new Error(
+      "Please specify a component name.\n\nUsage: npm run generate-component NewComponent\n"
+    );
   }
-} else {
-  console.log("This component already exists. Please choose a different name.");
-  process.exit(-1);
-}
 
-// Array of file template functions and their corresponding filenames
-const templates = [
-  [componentTemplate, `${PATH_PREFIX}/${fileName}.js`],
-  [indexTemplate, `${PATH_PREFIX}/index.js`],
-  [storyTemplate, `${PATH_PREFIX}/${fileName}.stories.js`],
-  [testTemplate, `${PATH_PREFIX}/${fileName}.test.js`]
-];
+  const fileName = process.argv[2];
 
-// Generate files with templates
-async function generateFiles() {
-  for (let i = 0; i < templates.length; i++) {
+  const PATH_PREFIX = `src/components/${fileName}`;
+
+  // Create the directories if the component name doesn't already exist
+  if (!fs.existsSync(`src/components/${fileName}`)) {
     try {
-      await writeFile(templates[i][1], templates[i][0](fileName), "utf8");
+      fs.mkdirSync(`src/components/${fileName}`);
+      fs.mkdirSync(`src/components/${fileName}/components`);
+    } catch (err) {
+      throw new Error(err);
+    }
+  } else {
+    throw new Error(
+      "This component already exists. Please choose a different name."
+    );
+  }
+
+  // Array of file template functions and their corresponding filenames
+  const templates = [
+    [componentTemplate, `${PATH_PREFIX}/${fileName}.js`],
+    [indexTemplate, `${PATH_PREFIX}/index.js`],
+    [storyTemplate, `${PATH_PREFIX}/${fileName}.stories.js`],
+    [testTemplate, `${PATH_PREFIX}/${fileName}.test.js`]
+  ];
+
+  // Generate files with templates
+  async function generateFiles() {
+    for (let i = 0; i < templates.length; i++) {
+      try {
+        await writeFile(templates[i][1], templates[i][0](fileName), "utf8");
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }
+
+  // Read in the entry file at src/components/index.js and update the
+  // the import/export statements with the newly generated component
+  async function updateEntryFile() {
+    try {
+      const entryFile = await readFile(
+        path.resolve("src", "components", "index.js"),
+        "utf8"
+      );
+
+      // Split the entry file on new lines
+      let updatedEntryFile = entryFile.split("\n");
+
+      // Add the import to the top of the file
+      updatedEntryFile = [
+        `import ${fileName} from "./${fileName}"`,
+        ...updatedEntryFile
+      ];
+
+      // Grab the index of the exported object
+      const exportIndex = updatedEntryFile.indexOf("const library = {") + 1;
+
+      // Add the export statement
+      updatedEntryFile.splice(exportIndex, 0, `${fileName},`);
+
+      // Write the file to disk
+      writeFile(
+        path.resolve("src", "components", "index.js"),
+        updatedEntryFile.join("\n"),
+        "utf8"
+      );
     } catch (err) {
       throw new Error(err);
     }
   }
-}
 
-// Read in the entry file at src/components/index.js and update the
-// the import/export statements with the newly generated component
-async function updateEntryFile() {
-  try {
-    const entryFile = await readFile(
-      path.resolve("src", "components", "index.js"),
-      "utf8"
-    );
-
-    // Split the entry file on new lines
-    let updatedEntryFile = entryFile.split("\n");
-
-    // Add the import to the top of the file
-    updatedEntryFile = [
-      `import ${fileName} from "./${fileName}"`,
-      ...updatedEntryFile
-    ];
-
-    // Grab the index of the exported object
-    const exportIndex = updatedEntryFile.indexOf("const library = {") + 1;
-
-    // Add the export statement
-    updatedEntryFile.splice(exportIndex, 0, `${fileName},`);
-
-    // Write the file to disk
-    writeFile(
-      path.resolve("src", "components", "index.js"),
-      updatedEntryFile.join("\n"),
-      "utf8"
-    );
-  } catch (err) {
-    throw new Error(err);
+  generateFiles();
+  updateEntryFile();
+} catch (err) {
+  if (err.message) {
+    console.log(err.message);
   }
 }
-
-generateFiles();
-updateEntryFile();
