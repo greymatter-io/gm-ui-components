27a4829a90276c4e2cabcfcd593bdee7c9626210
Stuart Hanberg <shanberg@users.noreply.github.com>
2018-10-26 09:24:47 -0400

    feat: theme navcards (#363)
    * feat: themeing navcards
    
    * fix: adding missing file
    
    * fix: fixing tests

--- src/components/NavCard/components/NavCardTitle.test.js
+++ src/components/NavCard/components/NavCardTitle.test.js
@@ -2,9 +2,17 @@ import React from "react";
 import { shallow } from "enzyme";
 import NavCardTitle from "./NavCardTitle";
 
+const theme = {
+  OPACITY_15: "0.15",
+  RADIUS_05: "4px",
+  COLOR_BACKGROUND_B: "blue",
+  COLOR_BACKGROUND_C: "red",
+  FONT_WEIGHT_SEMIBOLD: "500"
+};
+
 describe("NavCardTitle", () => {
   it("matches snapshot", () => {
-    const aNavCardTitle = shallow(<NavCardTitle />);
+    const aNavCardTitle = shallow(<NavCardTitle theme={theme} />);
     expect(aNavCardTitle).toMatchSnapshot();
   });
 });
--- src/components/NavCard/components/NavCardWrapper.test.js
+++ src/components/NavCard/components/NavCardWrapper.test.js
@@ -2,9 +2,19 @@ import React from "react";
 import { shallow } from "enzyme";
 import NavCardWrapper from "./NavCardWrapper";
 
+const theme = {
+  OPACITY_15: "0.15",
+  RADIUS_05: "4px",
+  COLOR_BACKGROUND_B: "blue",
+  COLOR_BACKGROUND_C: "red",
+  COLOR_INTENT_HIGHLIGHT: "green",
+  FONT_WEIGHT_REGULAR: "500",
+  FONT_STACK_BASE: "sans-serif"
+};
+
 describe("NavCardWrapper", () => {
   it("matches snapshot", () => {
-    const aNavCardWrapper = shallow(<NavCardWrapper />);
+    const aNavCardWrapper = shallow(<NavCardWrapper theme={theme} />);
     expect(aNavCardWrapper).toMatchSnapshot();
   });
 });
