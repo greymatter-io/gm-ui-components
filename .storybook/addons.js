8862abc9ce24b5411a2571884bf2d20d55e6717b
kaitmore <kaitmore@gmail.com>
2019-01-16 14:43:32 -0500

    fix: comment everything out, remove .babelrc

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,7 +1,2 @@
-import "@storybook/addon-links/register";
-import "@storybook/addon-knobs/register";
-// import "@storybook/addon-a11y/register";
-import "@storybook/addon-options/register";
-import "@storybook/addon-viewport/register";
-import "@storybook/addon-backgrounds/register";
-import "storybook-addon-styled-component-theme/dist/src/register";
+import '@storybook/addon-actions/register';
+import '@storybook/addon-links/register';
