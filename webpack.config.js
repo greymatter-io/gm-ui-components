ee941283e69d2a6c15b244908c238edaa344a33a
kaitmore <kaitmore@gmail.com>
2019-01-17 15:45:10 -0500

    fix: remove file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -33,21 +33,14 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(jpg|gif)$/,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader"
-          }
-        ]
-      },
-      {
-        test: /\.(eot|png|jpg|gif|ttf|woff|woff2)$/,
+        test: /\.(eot|png|jpg|jpeg|gif|ttf|woff|woff2)$/,
         use: [
           {
             loader: "url-loader",
             options: {
               // Images larger than 10 KB won’t be inlined
+              // If the file is greater than the limit, file-loader is used by default and all query parameters are passed to it.
+              // Using an alternative to file-loader is enabled via the fallback option.
               limit: 10 * 1024
             }
           }
