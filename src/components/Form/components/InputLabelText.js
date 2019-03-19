01d85aafc4eaa32d75f192d6a2506ab7dc8c2df1
shanberg <shanberg@users.noreply.github.com>
2019-03-19 10:49:27 -0400

    fix: theme input label text color

--- src/components/Form/components/InputLabelText.js
+++ src/components/Form/components/InputLabelText.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import { FONT_SIZE_BASE } from "style/styleVariables";
 
 const InputLabelText = styled.p`
   margin: 0;
-  font-size: ${FONT_SIZE_BASE};
+  font-size: ${props => props.theme.FONT_SIZE_BASE};
 `;
 
 export default InputLabelText;
