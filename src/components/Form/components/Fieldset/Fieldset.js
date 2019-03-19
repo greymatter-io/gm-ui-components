6d590c9556a82e1b92469cc4e2aa98d84212322c
shanberg <shanberg@users.noreply.github.com>
2019-03-19 10:39:46 -0400

    feat: make fieldset use theme

--- src/components/Form/components/Fieldset/Fieldset.js
+++ src/components/Form/components/Fieldset/Fieldset.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
-import { COLOR_KEYLINE, RADIUS_1 } from "style/styleVariables";
 import { spacingScale } from "style/styleFunctions";
 
 const Fieldset = styled.fieldset`
-  border: 1px solid ${COLOR_KEYLINE};
+  border: 1px solid ${props => props.theme.COLOR_KEYLINE};
   padding: ${spacingScale(2)};
-  border-radius: ${RADIUS_1};
+  border-radius: ${props => props.theme.RADIUS_1};
 `;
 
 export default Fieldset;
