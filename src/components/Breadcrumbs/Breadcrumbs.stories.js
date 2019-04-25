42e29231c1c579e51b890dd87f1209ecfd2b005c
kaitmore <kaitmore@gmail.com>
2019-04-25 15:06:06 -0400

    test: update tests and add storybook story

--- src/components/Breadcrumbs/Breadcrumbs.stories.js
+++ src/components/Breadcrumbs/Breadcrumbs.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { array, number, boolean } from "@storybook/addon-knobs";
-import Breadcrumbs from "./Breadcrumbs";
+import { array, boolean } from "@storybook/addon-knobs";
+import { Breadcrumbs } from "../";
 
 const stories = storiesOf("Components|Breadcrumbs", module);
 const defaultCrumbs = [
@@ -12,22 +12,47 @@ const defaultCrumbs = [
   "Graphs"
 ];
 const breadCrumbsInfo =
-  "A React component that allows users to know their location. Use <Breadcrumbs> and pass it an array of crumbs and an optional maxItems prop.  If there are more children than the maximum, it will render a collapsed view.";
+  "A React component that allows users to know their location. Use <Breadcrumbs> and pass it an array of crumbs to render.  Breadcrumb items will expand or contract to fill any allowed space. Truncated breadcrumb items expand to full natural width when hovered or focused to reveal content.";
 
-stories.add(
-  "default",
-  () => {
-    return (
-      <Breadcrumbs
-        crumbs={array("crumbs", defaultCrumbs)}
-        maxItems={number("maxItems", 5)}
-        collapse={boolean("collapse", false)}
-      />
-    );
-  },
-  {
-    info: {
-      text: breadCrumbsInfo
+stories
+  .add(
+    "default",
+    () => {
+      return (
+        <Breadcrumbs
+          crumbs={array("crumbs", defaultCrumbs)}
+          hideDelimiter={boolean("hideDelimiter", false)}
+        />
+      );
+    },
+    {
+      info: {
+        text: breadCrumbsInfo
+      }
     }
-  }
-);
+  )
+  .add(
+    "with smaller container",
+    () => {
+      return (
+        <div
+          style={{
+            width: "350px",
+            border: "1px solid #ddd",
+            padding: "4px",
+            borderRadius: "2px"
+          }}
+        >
+          <Breadcrumbs
+            crumbs={array("crumbs", defaultCrumbs)}
+            hideDelimiter={boolean("hideDelimiter", false)}
+          />
+        </div>
+      );
+    },
+    {
+      info: {
+        text: breadCrumbsInfo
+      }
+    }
+  );
--- src/components/Breadcrumbs/Breadcrumbs.test.js
+++ src/components/Breadcrumbs/Breadcrumbs.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import renderer from "react-test-renderer";
-import { Breadcrumbs, BreadcrumbItem } from "./index.js";
 import { shallow } from "enzyme";
+import Breadcrumbs, { Breadcrumb } from "./Breadcrumbs";
 
 const crumbs = [
   "Test 1",
@@ -10,36 +9,36 @@ const crumbs = [
   "Testing a really long breadcrumb item"
 ];
 
-const TestExpanded = <Breadcrumbs maxItems={10} crumbs={crumbs} />;
-const TestCollapsed = <Breadcrumbs maxItems={3} crumbs={crumbs} />;
-
-const ExpandedBreadcrumbTree = renderer.create(TestExpanded).toJSON();
-const CollapsedBreadcrumbTree = renderer.create(TestCollapsed).toJSON();
-
-describe("<Breadcrumbs> Snapshot", () => {
-  test("renders expanded breadcrumbs correctly", () => {
-    expect(ExpandedBreadcrumbTree).toMatchSnapshot();
+describe("Breadcrumbs", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Breadcrumbs crumbs={crumbs} />).dive();
   });
-  test("renders collapsed breadcrumbs correctly", () => {
-    expect(CollapsedBreadcrumbTree).toMatchSnapshot();
+  test("matches snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  test("renders Breadcrumb component", () => {
+    expect(wrapper.find("Breadcrumb")).toHaveLength(4);
+  });
+  test("passes hideDelimiter prop to Breadcrumb", () => {
+    wrapper = shallow(
+      <Breadcrumbs crumbs={crumbs} hideDelimiter={true} />
+    ).dive();
+    expect(
+      wrapper
+        .find("Breadcrumb")
+        .at(0)
+        .props().hideDelimiter
+    ).toBeTruthy();
   });
 });
 
-describe("<Breadcrumbs>", () => {
+describe("Breadcrumb", () => {
   let wrapper;
-
   beforeEach(() => {
-    wrapper = shallow(TestCollapsed);
+    wrapper = shallow(<Breadcrumb />);
   });
-
-  test("collapse when children are greater than maxItems", () => {
-    expect(wrapper.state("isCollapsed")).toEqual(true);
-    expect(wrapper.find(BreadcrumbItem).length).toBe(3);
-  });
-
-  test("do not collapse when children are less than maxItems", () => {
-    wrapper.setProps({ maxItems: 5 });
-    expect(wrapper.state("isCollapsed")).toEqual(false);
-    expect(wrapper.find(BreadcrumbItem).length).toBe(4);
+  test("matches snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
   });
 });
