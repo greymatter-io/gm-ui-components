acebe2561f65781961c8d052febf43cbf5d974db
kaitmore <kaitmore@gmail.com>
2019-04-29 16:17:46 -0400

    fix: minor console errors

--- .storybook/config.js
+++ .storybook/config.js
@@ -18,7 +18,19 @@ function loadStories() {
 
 addParameters({
   options: {
-    theme: gmTheme
+    theme: gmTheme,
+    brandTitle: "Grey Matter UI Components",
+    brandUrl: "#",
+    isFullscreen: false,
+    showNav: true,
+    showPanel: true,
+    showSearchBox: false,
+    panelPosition: "bottom",
+    sortStoriesByKind: true,
+    chySeparator: /\//,
+    hierarchyRootSeparator: /\|/,
+    sidebarAnimations: false,
+    selectedAddonPanel: undefined // The order of addons in the "Addons Panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
   }
 });
 
@@ -55,69 +67,5 @@ addDecorator(story => <StoryContainer>{story()}</StoryContainer>);
 
 const themes = [keen, keenDark];
 addDecorator(withThemesProvider(themes));
-
-// Options:
-addDecorator(
-  withOptions({
-    /**
-     * name to display in the top left corner
-     * @type {String}
-     */
-    name: "Grey Matter UI Components",
-    /**
-     * URL for name in top left corner to link to
-     * @type {String}
-     */
-    url: "#",
-    /**
-     * show story component as full screen
-     * @type {Boolean}
-     */
-    goFullScreen: false,
-    /**
-     * display left panel that shows a list of stories
-     * @type {Boolean}
-     */
-    showStoriesPanel: true,
-    /**
-     * display horizontal panel that displays addon configurations
-     * @type {Boolean}
-     */
-    showAddonPanel: true,
-    /**
-     * display floating search box to search through stories
-     * @type {Boolean}
-     */
-    showSearchBox: false,
-    /**
-     * show horizontal addons panel as a vertical panel on the right
-     * @type {Boolean}
-     */
-    addonPanelInRight: false,
-    /**
-     * sorts stories
-     * @type {Boolean}
-     */
-    sortStoriesByKind: true,
-    /**
-     * regex for finding the hierarchy separator
-     * @example:
-     *   null - turn off hierarchy
-     *   /\// - split by `/`
-     *   /\./ - split by `.`
-     *   /\/|\./ - split by `/` or `.`
-     * @type {Regex}
-     */
-    hierarchySeparator: /\//,
-    hierarchyRootSeparator: /\|/,
-    sidebarAnimations: false,
-
-    /**
-     * id to select an addon panel
-     * @type {String}
-     */
-    selectedAddonPanel: undefined // The order of addons in the "Addons Panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
-  })
-);
 
 configure(loadStories, module);
