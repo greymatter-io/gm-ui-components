53bdf47d776a23380300bc2feb7aba45e56b8e5a
David Goldstein <dgoldstein01@gmail.com>
2019-05-09 14:39:33 -0400

    Release testing 2 (#440)
    * feat: export style variables and functions (#422)
    
    * feat: fluid CSS solution to the breadcrumb overflow problem (#402)
    
    * feat: fluid CSS solution to the breadcrumb overflow problem
    
    * fix: removing test styles
    
    * test: update snapshots
    
    * test: remove OBE tests
    
    * fix: clean up javascript, remove unneeded props
    
    * test: update tests and add storybook story
    
    * feat: customizable footer (#423)
    
    * feat: customizable footer
    
    * test: refactor AppFooter tests
    
    * fix: footer render statement
    
    * fix: breadcrumbs error (#428)
    
    * fix: error in breadcrumbs component
    
    * fix: use index in key
    
    * fix: blank page when clicking anchor links in styles story (#425)
    
    * feat: update storybook to 5 (#429)
    
    * feat: update storybook to 5
    
    * feat: give Readouts stories default color to prevent storybook crash
    
    * feat: add minimal grey matter theme
    
    * fix: minor console errors
    
    * chore: update storybook patch version
    
    * fix: workaround for spinner issue in chrome (#430)
    
    * fix: workaround for svg issue in chrome
    
    * test: update snapshots
    
    * docs: update contributing guide (#426)
    
    * Update contributing guide
    
    * Update considerations text
    
    * Update component guideline content
    
    * Clarify component naming guideline
    
    * Update guideline text
    
    * feat: add double chevrons for up and down directions (#431)
    
    * feat: add prop to buttons to allow custom color (#437)
    
    * feat: add prop to buttons to allow custom color
    
    * fix: use color instead of text for color prop
    
    * chore: bumped version

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,10 @@
-const webpackConfig = require("../webpack.config.js");
+const custom = require("../webpack.config.js");
 const path = require("path");
 
-// We only want to pull the resolve and module config from our root webpack.config.js
-module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "../src/components/index.js"),
-  resolve: webpackConfig.resolve,
-  module: webpackConfig.module
+module.exports = async ({ config, mode }) => {
+  return {
+    ...config,
+    resolve: { ...custom.resolve },
+    module: { ...config.module, rules: custom.module.rules }
+  };
 };
