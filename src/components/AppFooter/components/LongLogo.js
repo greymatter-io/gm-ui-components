1474fcc8e570d550fd6ba4ddfab6a19412e38630
shanberg <shanberg@users.noreply.github.com>
2019-05-29 13:35:39 -0400

    feat: leaner styles imposed on logo

--- src/components/AppFooter/components/LongLogo.js
+++ src/components/AppFooter/components/LongLogo.js
@@ -5,14 +5,12 @@ import { keen } from "style/styleVariables";
 const LongLogo = styled.a`
   padding: 0 ${spacingScale(1)};
   color: ${props => props.theme.COLOR_CONTENT};
-  opacity: ${props => props.theme.OPACITY_70};
   flex: 0 1 auto;
   text-align: left;
   transition: all 0.2s ease;
   filter: grayscale(100%);
 
   &:hover {
-    opacity: ${props => props.theme.OPACITY_100};
     color: ${props => props.theme.COLOR_CONTENT};
     transition: all 0.2s ease;
     filter: grayscale(0%);
