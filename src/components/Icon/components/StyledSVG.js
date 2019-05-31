d0985cafabd7fd9502106f7ac05dfa49da6f077a
shanberg <shanberg@users.noreply.github.com>
2019-05-31 10:51:29 -0400

    refactor: trying to make StyledSVG work again

--- src/components/Icon/components/StyledSVG.js
+++ src/components/Icon/components/StyledSVG.js
@@ -1,20 +1,24 @@
 import styled from "styled-components";
 import { PropTypes } from "prop-types";
 
-const ICON_VIEWBOX_SIZE = props => props.theme.SPACING_BASE * 3;
+// const Input = styled.input.attrs(({ inputType }) => ({
+//   type: inputType
+// }))`
 
-const StyledSVG = styled.svg.attrs({
+const StyledSVG = styled.svg.attrs(({ props }) => ({
   version: "1.1",
   xmlns: "http://www.w3.org/2000/svg",
   xmlnsXlink: "http://www.w3.org/1999/xlink",
-  viewBox: `0 0 ${ICON_VIEWBOX_SIZE} ${ICON_VIEWBOX_SIZE}`,
-  preserveAspectRatio: "xMaxYMax meet"
-})`
+  preserveAspectRatio: "xMaxYMax meet",
+  viewBox:
+    "0 0 " + props.theme.SPACING_SCALE * 3 + " " + props.theme.SPACING_SCALE * 3
+}))`
   z-index: 1;
   width: ${props => (props.size ? props.size : "1.714em")};
   height: ${props => (props.size ? props.size : "1.714em")};
   word-spacing: 0;
   vertical-align: middle;
+
   &:not(:root) {
     overflow: visible;
   }
