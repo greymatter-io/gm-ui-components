a503f8a39038206a92b2c1b464112a200575f4ec
Stuart Hanberg <shanberg@users.noreply.github.com>
2019-06-03 09:31:01 -0400

    feat: replace old AppFooter logo (#462)
    * feat: replace footer logo and update color usage
    
    * feat: leaner styles imposed on logo
    
    * refactor: reduce styles needed for footer logo
    
    * test: update snaps
    
    * fix: add missing alt tag

--- src/components/AppFooter/components/LongLogo.js
+++ src/components/AppFooter/components/LongLogo.js
@@ -4,21 +4,19 @@ import { keen } from "style/styleVariables";
 
 const LongLogo = styled.a`
   padding: 0 ${spacingScale(1)};
+  color: ${props => props.theme.COLOR_CONTENT};
   opacity: ${props => props.theme.OPACITY_70};
   flex: 0 1 auto;
-  text-align: left;
   transition: all 0.2s ease;
-  filter: grayscale(100%);
 
   &:hover {
     opacity: ${props => props.theme.OPACITY_100};
-    transition: all 0.2s ease;
-    filter: grayscale(0%);
   }
 
-  img {
-    max-width: 140px; /* Magic number tied to image size */
-    vertical-align: -3px;
+  svg {
+    max-width: 142px; /* Magic number that optimizes for preferable antialiasing */
+    height: auto;
+    width: 100%;
   }
 `;
 
