a446ca7c2f6b7df708a226ce99817a6a614a9260
shanberg <98312+shanberg@users.noreply.github.com>
2019-06-11 10:28:19 -0400

    refactor: appfooter uses updated theme

--- src/components/AppFooter/components/LongLogo.js
+++ src/components/AppFooter/components/LongLogo.js
@@ -4,13 +4,12 @@ import { keen } from "style/theme";
 
 const LongLogo = styled.a`
   padding: 0 ${spacingScale(1)};
-  color: ${props => props.theme.COLOR_CONTENT_DEFAULT};
-  opacity: ${props => props.theme.OPACITY_LIGHT};
+  color: ${props => props.theme.COLOR_CONTENT_MUTED};
   flex: 0 1 auto;
   transition: all 0.2s ease;
 
   &:hover {
-    opacity: ${props => props.theme.OPACITY_FULL};
+    color: ${props => props.theme.COLOR_CONTENT_CONTRAST};
   }
 
   svg {
