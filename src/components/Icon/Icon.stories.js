e5dd07b23a2159810c30f73b883f78f5d9167f7d
shanberg <98312+shanberg@users.noreply.github.com>
2019-06-14 15:52:22 -0400

    feat: improved stories for icon

--- src/components/Icon/Icon.stories.js
+++ src/components/Icon/Icon.stories.js
@@ -179,13 +179,8 @@ stories
     "Icon with Custom Glyph",
     () => {
       return (
-        <Icon
-          fillColor={color("glyphColor", "#444")}
-          borderColor={color("glyphColor", "#444")}
-          size={text("size", "100px")}
-          title="Custom Glyph"
-        >
-          <svg className="svg-icon" viewBox="0 0 20 20">
+        <Icon title="Custom Glyph">
+          <svg className="svg-icon" viewBox="0 0 24 24">
             <path d="M17.218,2.268L2.477,8.388C2.13,8.535,2.164,9.05,2.542,9.134L9.33,10.67l1.535,6.787c0.083,0.377,0.602,0.415,0.745,0.065l6.123-14.74C17.866,2.46,17.539,2.134,17.218,2.268 M3.92,8.641l11.772-4.89L9.535,9.909L3.92,8.641z M11.358,16.078l-1.268-5.613l6.157-6.157L11.358,16.078z" />
           </svg>
         </Icon>
@@ -197,25 +192,25 @@ stories
           "An Icon component that renders a custom glyph. Simply import { Icon } and wrap your custom svg."
       }
     }
-  )
-  .add("Glyph Gallery", () => {
-    return (
-      <GalleryIconList fontSize={text("Context font size", "inherit")}>
-        {glyphNames.map(glyph => {
-          let Glyph = Glyphs[glyph];
-          return (
-            <GalleryIconDemo key={glyph}>
-              <Glyph
-                size={text("size")}
-                fillColor={color("fillColor")}
-                fillOpacity={number("fillOpacity")}
-                borderColor={color("borderColor")}
-                borderWidth={text("borderWidth")}
-              />
-              <GalleryIconLabel>{glyph}</GalleryIconLabel>
-            </GalleryIconDemo>
-          );
-        })}
-      </GalleryIconList>
-    );
-  });
+  );
+// .add("Glyph Gallery", () => {
+//   return (
+//     <GalleryIconList fontSize={text("Context font size", "inherit")}>
+//       {glyphNames.map(glyph => {
+//         let Glyph = Glyphs[glyph];
+//         return (
+//           <GalleryIconDemo key={glyph}>
+//             <Glyph
+//               size={text("size")}
+//               fillColor={color("fillColor")}
+//               fillOpacity={number("fillOpacity")}
+//               borderColor={color("borderColor")}
+//               borderWidth={text("borderWidth")}
+//             />
+//             <GalleryIconLabel>{glyph}</GalleryIconLabel>
+//           </GalleryIconDemo>
+//         );
+//       })}
+//     </GalleryIconList>
+//   );
+// })
