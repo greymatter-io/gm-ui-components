ae9b8bfd5d6fd4aeecddbbcd455821122def88b0
shanberg <98312+shanberg@users.noreply.github.com>
2019-06-20 14:13:14 -0400

    chore: update from develop

---
+++ src/components/Collapse/Collapse.stories.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Collapse from "./Collapse";
+
+const stories = storiesOf("Components|Collapse", module);
+
+const style = {
+  height: "300px",
+  width: "100%",
+  textAlign: "center",
+  backgroundColor: "lightgrey"
+};
+
+stories
+  .add(
+    "Default",
+    () => {
+      return (
+        <>
+          <Collapse title="Collapse Title" detail="Detail text">
+            <div style={style}>Collapse content</div>
+          </Collapse>
+        </>
+      );
+    },
+    {
+      info: {
+        text: "Add component description here. Accepts markdown."
+      }
+    }
+  )
+  .add(
+    "Multiple",
+    () => {
+      return (
+        <>
+          <Collapse title="Collapse Title" detail="Detail text">
+            <div style={style}>Collapse content</div>
+          </Collapse>
+          <Collapse title="Collapse Title" detail="Detail text">
+            <div style={style}>Collapse content</div>
+          </Collapse>
+          <Collapse title="Collapse Title" detail="Detail text">
+            <div style={style}>Collapse content</div>
+          </Collapse>
+        </>
+      );
+    },
+    {
+      info: {
+        text: "Add component description here. Accepts markdown."
+      }
+    }
+  );
