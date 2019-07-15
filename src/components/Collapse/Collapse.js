a2a6e1de6fbabe9dca5b31a71ec1ee849af2d447
kaitmore <kaitmore@gmail.com>
2019-07-15 19:16:18 -0400

    chore: remove lodash

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { PropTypes } from "prop-types";
-import _ from "lodash";
 import { IconChevronRight } from "components/Glyphs";
 import { keen } from "style/theme";
 
