ed2c7e35a51aa79137da79098e7eb31045481d11
dgoldstein1 <david.goldstein@deciphernow.com>
2019-08-06 12:55:29 -0400

    test: added snapshots

--- src/components/util/InputTextField.test.js
+++ src/components/util/InputTextField.test.js
@@ -7,4 +7,12 @@ describe("InputTextField", () => {
     const aInputTextField = shallow(<InputTextField />);
     expect(aInputTextField).toMatchSnapshot();
   });
+  it("displays danger color on invalid", () => {
+    const aInputTextField = shallow(<InputTextField invalid={true} />);
+    expect(aInputTextField).toMatchSnapshot();
+  })
+  it("displays success color on valid", () => {
+    const aInputTextField = shallow(<InputTextField valid={true} />);
+    expect(aInputTextField).toMatchSnapshot();
+  })
 });
