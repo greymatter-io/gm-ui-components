f529bb573486db4b6e6c37d2f00ac6aae7d9738b
shanberg <98312+shanberg@users.noreply.github.com>
2019-08-27 09:39:52 -0400

    test: blocking in tests

--- src/components/Status/Status.test.js
+++ src/components/Status/Status.test.js
@@ -1,5 +1,7 @@
 import React from "react";
+import "jest-styled-components";
 
+import { keen } from "style/theme";
 
 import Status from "./Status";
 
@@ -9,3 +11,26 @@ describe("Status", () => {
     expect(aStatus).toMatchSnapshot();
   });
 });
+
+describe("Layout", () => {
+  const aDefaultStatus = shallow(<Status theme={keen} />);
+  expect(aDefaultStatus.find("Wrap")).toHaveStyleRule("display", "grid");
+  expect(aDefaultStatus.find("Graphic").html()).toContain(
+    "color",
+    keen.COLOR_INTENT_HIGHLIGHT
+  );
+
+  const aBlockComponent = shallow(<Status isBlock />);
+  // expect(aBlockComponent).toHaveStyleRule("display", "flex");
+
+  const anErrorStatus = shallow(<Status statusType="error" />);
+  // expect(anErrorStatus).toHaveStyleRule("display", "flex");
+});
+
+// if there's a message, it shows a message.
+
+// if there's a detail, it shows the detail.
+
+// if it's status: warning, it uses the warning color.
+
+// if there's a custom graphic, it shows the graphic.
