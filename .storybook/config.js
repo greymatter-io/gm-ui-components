3192301ccfc4d5663b3de20238ed294291b678d4
shanberg <98312+shanberg@users.noreply.github.com>
2019-09-20 09:17:53 -0400

    feat: toss addon-info and replace with addon-docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -37,33 +37,6 @@ addParameters({
   }
 });
 
-// addon-info. This should always be the first decorator.
-addDecorator(
-  withInfo({
-    header: true, // Toggles display of header with component name and description
-    inline: true, // Displays info inline vs click button to view
-    source: true, // Displays the source of story Component
-    propTables: [
-      /* Components used in story */
-    ], // displays Prop Tables with this components
-    propTablesExclude: [], // Exclude Components from being shoun in Prop Tables section
-    styles: {
-      infoBody: {
-        background: "transparent",
-        fontFamily: "inherit",
-        color: "inherit",
-        opacity: "0.9",
-        border: "0"
-      }
-    }, // Overrides styles of addon
-    TableComponent: PropTypesTable,
-    components: {}, // Overrides components used to display markdown. Warning! This option's name will be likely deprecated in favor to "components" with the same API in 3.3 release. Follow this PR #1501 for details
-    maxPropsIntoLine: 1, // Max props to display per line in source code
-    maxPropObjectKeys: 10,
-    maxPropArrayLength: 10,
-    maxPropStringLength: 100
-  })
-);
 // add withKnobs globally so we don't need to import it into individual stories
 addDecorator(withKnobs);
 
--- src/ComponentLibrary/StoryComponents/StoryContainer.js
+++ src/ComponentLibrary/StoryComponents/StoryContainer.js
@@ -4,51 +4,12 @@ import styled from "styled-components";
 import { keen } from "style/theme";
 import GlobalStyles from "style/GlobalStyles";
 
-const Wrap = styled.div`
-  width: 100%;
-  display: flex;
-  flex: 1 1 100%;
-  align-items: stretch;
-  justify-content: stretch;
-
-  > div {
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-
-    > div {
-      /* Header container */
-      &:nth-child(1) {
-        align-self: stretch;
-        background-color: #fff;
-        color: rgba(0, 0, 0, 0.8);
-      }
-      /* Story container */
-      &:nth-child(2) {
-        z-index: 10;
-        position: relative;
-        padding: 2rem;
-        background-color: ${({theme}) => theme.COLOR_BACKGROUND_DEFAULT};
-        box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.08);
-        margin-left: auto;
-        margin-right: auto;
-      }
-      /* Documentation container */
-      &:nth-child(3) {
-        align-self: stretch;
-        background-color: #fff;
-        color: rgba(0, 0, 0, 0.8);
-      }
-    }
-  }
-`;
-
 function StoryContainer({ children }) {
   return (
-    <Wrap>
+    <>
       <GlobalStyles />
       {children}
-    </Wrap>
+    </>
   );
 }
 
