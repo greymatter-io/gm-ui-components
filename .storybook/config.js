2ccd36f06d9737f404afc307394b7cfbd56de9c5
shanberg <98312+shanberg@users.noreply.github.com>
2019-09-20 13:40:59 -0400

    chore: cleanup storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,12 +3,14 @@ import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
 import { withKnobs } from "@storybook/addon-knobs";
 import { withThemesProvider } from "storybook-addon-styled-component-theme";
-import gmTheme from "./gmTheme";
 
+import gmTheme from "./gmTheme";
 import StoryContainer from "ComponentLibrary/StoryComponents/StoryContainer";
 import { keen, keenDark } from "style/theme";
 
-configure(require.context("../src/", true, /\.stories\.js$/), module);
+const themes = [keen, keenDark];
+
+configure(require.context("../src", true, /\.stories\.js$/), module);
 
 addParameters({
   options: {
@@ -17,8 +19,8 @@ addParameters({
     brandUrl: "#",
     isFullscreen: false,
     showNav: true,
-    showPanel: true,
-    showSearchBox: false,
+    showPanel: false,
+    showSearchBox: true,
     panelPosition: "bottom",
     sortStoriesByKind: true,
     chySeparator: /\//,
@@ -27,15 +29,7 @@ addParameters({
     selectedAddonPanel: undefined // The order of addons in the "Addons Panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
   }
 });
-
-// add withKnobs globally so we don't need to import it into individual stories
 addDecorator(withKnobs);
-
 addDecorator(withA11y);
-
 addDecorator(story => <StoryContainer>{story()}</StoryContainer>);
-
-const themes = [keen, keenDark];
 addDecorator(withThemesProvider(themes));
-
-configure(loadStories, module);
