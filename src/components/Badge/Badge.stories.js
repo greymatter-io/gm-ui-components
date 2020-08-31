34d2ee511538b317d21afed10949b5989aeaadbe
dgoldstein1 <david.goldstein@deciphernow.com>
2020-08-31 15:49:43 -0500

    chore: linting

--- src/components/Badge/Badge.stories.js
+++ src/components/Badge/Badge.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { array, boolean, text, color } from "@storybook/addon-knobs";
+import { text, color } from "@storybook/addon-knobs";
 import Badge from "./Badge";
 
 const stories = storiesOf("Components|Badge", module);
