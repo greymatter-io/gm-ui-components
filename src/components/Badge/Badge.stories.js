c5295a0b28cfb2786542d5b4ca50271df46325d4
dgoldstein1 <david.goldstein@deciphernow.com>
2020-08-31 16:58:43 -0500

    chore: removed text box badge story

--- src/components/Badge/Badge.stories.js
+++ src/components/Badge/Badge.stories.js
@@ -5,31 +5,10 @@ import Badge from "./Badge";
 
 const stories = storiesOf("Components|Badge", module);
 
-stories
-  .add(
-    "Default",
-    () => {
-      return (
-        <Badge color={color("blue")}>{text("label", "Hello World")}</Badge>
-      );
-    },
-    {}
-  )
-  .add(
-    "With limited container size",
-    () => {
-      return (
-        <div
-          style={{
-            width: "350px",
-            border: "1px solid #ddd",
-            padding: "4px",
-            borderRadius: "2px"
-          }}
-        >
-          <Badge color={color("blue")}>{text("label", "Hello World")}</Badge>
-        </div>
-      );
-    },
-    {}
-  );
+stories.add(
+  "Default",
+  () => {
+    return <Badge color={color("blue")}>{text("label", "Hello World")}</Badge>;
+  },
+  {}
+);
