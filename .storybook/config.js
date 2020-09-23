196177f36ea5a3acf7ed3d19b51a700218fb81c0
kaitmore <kaitmore@gmail.com>
2020-09-23 09:03:29 -0400

    fix: remove unused imports

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,18 +2,13 @@ import React from "react";
 import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
 import { withInfo } from "@storybook/addon-info";
-import { withOptions } from "@storybook/addon-options";
 import { withKnobs } from "@storybook/addon-knobs";
-import { setDefaults } from "react-storybook-addon-props-combinations";
 import { withThemesProvider } from "storybook-addon-styled-component-theme";
 import gmTheme from "./gmTheme";
 
 import PropTypesTable from "./PropTypesTable";
 import StoryContainer from "ComponentLibrary/StoryComponents/StoryContainer";
 import { keen, keenDark } from "style/theme";
-import { transparentize } from "polished";
-
-require("typeface-inter");
 
 const req = require.context("../src/", true, /\.stories\.js$/);
 
