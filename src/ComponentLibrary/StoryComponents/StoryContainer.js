5e06bd0317a3bdec35a7815b1fde2568ac188316
Kait Moreno <kaitlin.moreno@deciphernow.com>
2020-11-25 15:59:28 -0500

    chore: minor and patch updates (#605)

--- src/ComponentLibrary/StoryComponents/StoryContainer.js
+++ src/ComponentLibrary/StoryComponents/StoryContainer.js
@@ -7,38 +7,34 @@ import GlobalStyles from "style/GlobalStyles";
 const Wrap = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: column;
   flex: 1 1 100%;
   align-items: stretch;
   justify-content: stretch;
 
   > div {
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-
-    > div {
-      /* Header container */
-      &:nth-child(1) {
-        align-self: stretch;
-        background-color: #fff;
-        color: rgba(0, 0, 0, 0.8);
-      }
-      /* Story container */
-      &:nth-child(2) {
-        z-index: 10;
-        position: relative;
-        padding: 2rem;
-        background-color: ${({ theme }) => theme.COLOR_BACKGROUND_DEFAULT};
-        box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.08);
-        margin-left: auto;
-        margin-right: auto;
-      }
-      /* Documentation container */
-      &:nth-child(3) {
-        align-self: stretch;
-        background-color: #fff;
-        color: rgba(0, 0, 0, 0.8);
-      }
+    /* Header container */
+    &:nth-child(1) {
+      align-self: stretch;
+      background-color: #fff;
+      color: rgba(0, 0, 0, 0.8);
+    }
+    /* Story container */
+    &:nth-child(2) {
+      z-index: 10;
+      align-self: center;
+      position: relative;
+      padding: 2rem;
+      background-color: ${({ theme }) => theme.COLOR_BACKGROUND_DEFAULT};
+      box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.08);
+      margin-left: auto;
+      margin-right: auto;
+    }
+    /* Documentation container */
+    &:nth-child(3) {
+      align-self: stretch;
+      background-color: #fff;
+      color: rgba(0, 0, 0, 0.8);
     }
   }
 `;
