74b25af59913fd0c1061f4f6f887886294185f86
David Tillery <david.tillery@deciphernow.com>
2020-12-01 17:24:32 -0500

    small tweaks (#613)
    * fix: remove odd background colors on theme fieldsets
    
    * fix: update deprecation warning
    
    * chore: replace and document backgrounds for fieldsets in theme story
    
    * test: fix lint errors
    
    * chore: update icon deprecation with version
    
    Co-authored-by: shanberg <98312+shanberg@users.noreply.github.com>

--- src/style/themes.stories.js
+++ src/style/themes.stories.js
@@ -29,22 +29,44 @@ const DemoCols = styled.div`
 
 const DemoCanvas = styled.div`
   padding: 1rem;
+  border: 1px solid ${({ theme }) => theme.COLOR_KEYLINE_DEFAULT};
+  border-radius: ${({ theme }) => theme.CORNER_RADIUS_CARD_LG};
   background-color: ${({ theme }) => theme.COLOR_BACKGROUND_DEFAULT};
   color: ${({ theme }) => theme.COLOR_CONTENT_DEFAULT};
+  position: relative;
 `;
 
-const StyledFieldset = styled(Fieldset)`
-  background-color: ${({ theme }) => theme.COLOR_BACKGROUND_TWO};
-`;
-
-const StyledFieldset2 = styled(Fieldset)`
-  background-color: ${({ theme }) => theme.COLOR_BACKGROUND_THREE};
+const DemoFieldset = styled(Fieldset)`
+  position: relative;
 `;
 
 const Space = styled.span`
   display: inline-block;
   height: ${spacingScale(1)};
   width: ${spacingScale(1)};
+`;
+
+const BackgroundAreaTwo = styled(Fieldset)`
+  background-color: ${({ theme }) => theme.COLOR_BACKGROUND_TWO};
+  position: relative;
+  overflow: hidden;
+`;
+
+const BackgroundAreaThree = styled(Fieldset)`
+  background-color: ${({ theme }) => theme.COLOR_BACKGROUND_THREE};
+  position: relative;
+  overflow: hidden;
+`;
+
+const Note = styled.span`
+  display: block;
+  text-transform: uppercase;
+  font-size: ${({ theme }) => theme.FONT_SIZE_TEXT_XS};
+  letter-spacing: 0.03em;
+  color: ${({ theme }) => theme.COLOR_CONTENT_NONESSENTIAL};
+  position: absolute;
+  bottom: 0.5rem;
+  right: 0.5rem;
 `;
 
 const DocumentationLink = styled.h2`
@@ -61,6 +83,7 @@ const DocumentationLink = styled.h2`
 function DemoContent({ themeName }) {
   return (
     <DemoCanvas>
+      <Note>Background level one</Note>
       <Breadcrumbs crumbs={["Overview", "Themes", "Theme"]} />
       <Space />
       <h1>{themeName}</h1>
@@ -75,7 +98,8 @@ function DemoContent({ themeName }) {
         <Tab label="Label" />
         <Tab label="Disabled" disabled />
       </TabGroup>
-      <Fieldset>
+      <DemoFieldset>
+        <Note>Background level three</Note>
         <Textarea
           autoFocus={false}
           label="Textarea"
@@ -97,24 +121,26 @@ function DemoContent({ themeName }) {
         <Space />
         <Checkbox labelPosition="right" label={"Checkbox"} />
         <Radio labelPosition="right" label={"Radio"} />
-      </Fieldset>
+      </DemoFieldset>
 
       <Space />
-      <StyledFieldset>
+      <BackgroundAreaTwo>
+        <Note>Background level two</Note>
         <ButtonGroup>
           <Button label={"Plain Button"} />
           <Space />
           <Button type={"primary"} label={"Primary Button"} />
         </ButtonGroup>
-      </StyledFieldset>
+      </BackgroundAreaTwo>
       <Space />
-      <StyledFieldset2>
+      <BackgroundAreaThree>
+        <Note>Background level three</Note>
         <ButtonGroup>
           <Button label={"Plain Button"} />
           <Space />
           <Button type={"primary"} label={"Primary Button"} />
         </ButtonGroup>
-      </StyledFieldset2>
+      </BackgroundAreaThree>
       <Space />
     </DemoCanvas>
   );
